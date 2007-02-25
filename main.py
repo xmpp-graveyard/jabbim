68f2ec05dc6f6f8dd32c070646e6eca5a9e686f4
hanz <hanz@users.noreply.github.com>
2007-02-25 14:34:33 +0000

    oprava nevymazavani polozek z events

--- main.py
+++ main.py
@@ -33,87 +33,13 @@ from status import *
 from chatwindow import *
 from joingroupchat import *
 from addcontact import *
+from grouped_events import *
 from discovery_register import *
 from discovery_ui import *
-from grouped_events_ui import *
+
 from vcard_ui import *
 
 #import games
-
-class groupedEventWindow(QtGui.QMainWindow):
-	def __init__(self,parent,main,jab):
-		apply(QtGui.QMainWindow.__init__,(self,parent))
-		self.main=main
-		self.jab=jab
-		self.ui=Ui_groupedevents()
-		self.ui.setupUi(self)
-
-		self.subscriptions=QtGui.QTreeWidgetItem(self.ui.events)
-		self.subscriptions.setText(0,self.tr("Subscriptions"))
-		self.subscribed=QtGui.QTreeWidgetItem(self.ui.events)
-		self.subscribed.setText(0,self.tr("Subscribed"))
-
-		self.events={}
-		QtCore.QObject.connect(self.ui.events, QtCore.SIGNAL("itemClicked ( QTreeWidgetItem * , int )"),self.eventClicked)
-		QtCore.QObject.connect(self.ui.events, QtCore.SIGNAL("itemSelectionChanged ()"),self.eventSelected)
-		QtCore.QObject.connect(self.ui.addContact, QtCore.SIGNAL("clicked()"),self.add)
-		QtCore.QObject.connect(self.ui.ok, QtCore.SIGNAL("clicked()"),self.ok)
-		self.text=self.tr("Use Jabber ID as name")
-		self.ui.nickname.addItem(self.text)
-
-	def ok(self):
-		for item in self.ui.events.selectedItems():
-			if item.parent()!=None:
-				self.subscribed.takeChild(self.subscribed.indexOfChild(item))
-
-	def add(self):
-		for i in range(self.ui.selectedUsers.count()):
-			item=self.ui.selectedUsers.item(i)
-			jid=unicode(item.text())
-			self.jab.roster.Authorize(jid)
-			if not self.main.ui.roster.isUser(jid):
-				if self.ui.nickname.currentText()==self.text:
-					nickname=jid
-				else:
-					nickname=unicode(self.ui.nickname.currentText())
-				group=unicode(self.ui.group.currentText())
-				print "adding",jid,nickname,group
-				if len(group)!=0:
-					if self.main.groups.has_key(group):
-						self.main.groups[group]["users"][str(jid)]={"item":self.main.ui.roster.addUser(jid,nickname,self.main.groups[group]["item"],self.main.offline,self.main.statuses["offline"]),"resources":[]}
-					else:
-						self.main.groups[group]={"item":self.main.ui.roster.addGroup(group),"users":{}}
-						self.main.groups[group]["users"][str(jid)]={"item":self.main.ui.roster.addUser(jid,nickname,self.main.groups[group]["item"],self.main.offline,self.main.statuses["offline"]),"resources":[]}
-				else:
-					self.main.groups["Unknown"]["users"][str(jid)]={"item":self.main.ui.roster.addUser(jid,nickname,self.main.groups["Unknown"]["item"],self.main.offline,self.main.statuses["offline"]),"resources":[]}
-				self.jab.roster.setItem(jid,nickname,[group])
-				self.jab.roster.Subscribe(jid)
-			for x in range(self.subscriptions.childCount()):
-				if unicode(self.subscriptions.child(x).text(0))==jid:
-					self.subscriptions.takeChild(x)
-
-	def eventSelected(self):
-		self.ui.selectedUsers.clear()
-		for item in self.ui.events.selectedItems():
-			if item.parent()!=None:
-				self.ui.selectedUsers.addItem(item.text(0))
-
-	def eventClicked(self,item,i):
-		if item.parent()==self.subscriptions:
-			self.ui.stackedWidget.setCurrentIndex(0)
-		elif item.parent()==self.subscribed:
-			self.ui.stackedWidget.setCurrentIndex(1)
-		
-	def addEvent(self,typ,data):
-		self.ui.group.clear()
-		for k,v in self.main.groups.iteritems():
-			self.ui.group.addItem(unicode(k))
-		if typ=="subscribe":
-			self.events[data["jid"]]=QtGui.QTreeWidgetItem(self.subscriptions)
-			self.events[data["jid"]].setText(0,data["jid"])
-		elif typ=="subscribed":
-			self.events[data["jid"]]=QtGui.QTreeWidgetItem(self.subscribed)
-			self.events[data["jid"]].setText(0,data["jid"])
 
 class vcardWindow(QtGui.QDialog):
 	def __init__(self,parent=None,vcard=None,readonly=True):
@@ -583,7 +509,7 @@ class mainWindow(QtGui.QMainWindow):
 			self.events.show()
 			self.events.addEvent("subscribed",{"jid":str(jid)})
 			if not self.ui.roster.isUser(jid):
-				self.groups["Unknown"]["users"][str(jid)]={"item":self.ui.roster.addUser(jid,nickname,self.groups["Unknown"]["item"],self.offline,self.statuses["offline"]),"resources":[]}
+				self.groups["Unknown"]["users"][str(jid)]={"item":self.ui.roster.addUser(jid,jid,self.groups["Unknown"]["item"],self.offline,self.statuses["offline"]),"resources":[]}
 
 		elif e[0] == "subscribe":
 			jid=str(e[1])
