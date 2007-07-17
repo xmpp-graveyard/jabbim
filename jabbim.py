4cbe4dba9d761002b0f9e29c7947b4960524881a
hanz <hanz@users.noreply.github.com>
2007-07-17 05:24:34 +0000

    dalsi upravy kvuli segfaultum

--- jabbim.py
+++ jabbim.py
@@ -352,7 +352,8 @@ class clientClass(pyxl.client.Client):
 		log.msg("delete contact")
 		#contact=self.roster['users'][jid]
 		items=self.main.ui.roster.getUserItems(jid)
-		for item in items:
+		for i in range(len(items)):
+			item=self.main.ui.roster.getUserItems(jid)[0]
 			log.msg("DELETE ITEM:"+unicode(item.text(1)))
 			parent=item.parent()
 			parent.takeChild(parent.indexOfChild(item))
