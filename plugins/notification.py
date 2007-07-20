f6623ddb427861104ff3e051c2817b3c72844266
hanz <hanz@users.noreply.github.com>
2007-07-20 19:53:03 +0000

    zacatek podporu pluginu

---
+++ plugins/notification.py
@@ -0,0 +1,21 @@
+try:
+	from PyQt4 import QtCore, QtGui
+except:
+	print "PyQt4 is not installed."
+
+class plugin():
+	def __init__(self,main):
+		self.main=main
+		self.main.client.dispatcher.registerHandler('on_message', self.on_message)
+
+	def on_message(self,frm,typ,body,subject, xhtml,  chatstate,  delay):
+		#print "message!!!!!!!"
+		if self.main.client.roster['users'].has_key(frm):
+			user=self.main.client.roster['users'][frm].name
+		else:
+			user=frm
+		if len(body)>40:
+				traytext=body[:40]+" ..."
+		else:
+				traytext=body
+		self.main.tray.showMessage(self.main.tr("New message from ")+unicode(user), traytext, QtGui.QSystemTrayIcon.Information, 5000)
