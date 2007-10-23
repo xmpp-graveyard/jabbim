bd6d6dd9ae059198d5bcf0a638b04327c413c4a3
hanz <hanz@users.noreply.github.com>
2007-10-23 06:47:04 +0000

    zacatek buttonu v discovery

--- widgets/servicediscovery.py
+++ widgets/servicediscovery.py
@@ -11,7 +11,7 @@ import legacyforms
 class serviceDiscoveryDialog(QtGui.QDialog):
 	def __init__(self,main,parent=None):
 		apply(QtGui.QDialog.__init__,(self,parent))
-		self.setModal(True)
+		self.setModal(False)
 		self.ui=Ui_serviceDiscovery()
 		self.ui.setupUi(self)
 		self.main=main
@@ -24,6 +24,9 @@ class serviceDiscoveryDialog(QtGui.QDialog):
 		QtCore.QObject.connect(self.ui.tree, QtCore.SIGNAL("itemDoubleClicked ( QTreeWidgetItem * , int )"),self.itemClicked)
 		QtCore.QObject.connect(self.ui.tree, QtCore.SIGNAL("itemClicked ( QTreeWidgetItem *, int)"),self.itemSelected)
 		QtCore.QObject.connect(self.ui.register, QtCore.SIGNAL("clicked()"),self.register)
+		QtCore.QObject.connect(self.ui.search, QtCore.SIGNAL("clicked()"),self.search)
+		self.group=QtGui.QButtonGroup(self)
+		QtCore.QObject.connect(self.group,QtCore.SIGNAL("buttonClicked ( QAbstractButton * )"),self.buttonClicked)
 
 #d=self.main.client.getRegisterForm(jid)
 		self.load()
@@ -38,11 +41,32 @@ class serviceDiscoveryDialog(QtGui.QDialog):
 			#elif self.main.client.disco[key][None].has_key("err"):
 				#print key,"error"
 
+	def buttonClicked(self,b):
+		if b.typ=="register":
+			d=self.main.client.getRegisterForm(unicode(b.jid))
+			d.addCallback(self._onRegister)
+		elif b.typ=="search":
+			d=self.main.client.getSearchForm(b.jid)
+			d.addCallback(self._gotSearchForm)
+
 	def register(self):
-		jid=unicode(self.ui.tree.currentItem().text(1))
+		jid=unicode(self.ui.tree.currentItem().text(3))
 		d=self.main.client.getRegisterForm(jid)
 		d.addCallback(self._onRegister)
-	
+
+	def search(self):
+		jid=unicode(self.ui.tree.currentItem().text(3))
+		d=self.main.client.getSearchForm(jid)
+		d.addCallback(self._gotSearchForm)
+
+	def _gotSearchForm(self,data):
+		if not data:
+			return
+		jid,legacy,form=data
+		if form!=None:
+			self.dialog=dataforms.dataFormsDialog(self.main,form,jid,"Search",self)
+			self.dialog.show()
+
 	def _onRegister(self,data):
 		if not data:
 			return
@@ -56,12 +80,16 @@ class serviceDiscoveryDialog(QtGui.QDialog):
 
 	def expanded(self,item):
 		self.ui.tree.resizeColumnToContents(0)
+		#self.ui.tree.resizeColumnToContents(1)
+		#self.ui.tree.resizeColumnToContents(2)
 
 	def collapsed(self,item):
 		self.ui.tree.resizeColumnToContents(0)
+		#self.ui.tree.resizeColumnToContents(1)
+		#self.ui.tree.resizeColumnToContents(2)
 
 	def itemClicked(self,item,i):
-		jid=unicode(item.text(1))
+		jid=unicode(item.text(3))
 		if len(jid)==0:
 			return
 		self.main.client.getDiscoItems(jid, callback = self._discoItemsReceived, callback_par = (item))
@@ -74,12 +102,13 @@ class serviceDiscoveryDialog(QtGui.QDialog):
 
 	def itemSelected(self,item,i):
 		self.ui.register.setEnabled(self.hasFeature(item,"jabber:iq:register"))
+		self.ui.search.setEnabled(self.hasFeature(item,"jabber:iq:search"))
 
 	def _discoItemsReceived(self,item):
-		jid=unicode(item.text(1))
+		jid=unicode(item.text(3))
 		for key,values in self.main.client.disco[jid][None]['items'].iteritems():
 			it=QtGui.QTreeWidgetItem(item)
-			it.setText(1,values['jid'])
+			it.setText(3,values['jid'])
 			if values.has_key("name"):
 				it.setText(0,values["name"])
 			else:
@@ -112,16 +141,37 @@ class serviceDiscoveryDialog(QtGui.QDialog):
 								elif typ=="file":
 									typ="disk"
 								parentitem.setIcon(0,self.main.getIcon(size="16x16",usertype=typ))
-								parentitem.setText(1,key)
+								parentitem.setText(3,key)
 				if self.main.client.disco[key][None].has_key("features"):
 					parentitem.setData(32,0,QtCore.QVariant(list(self.main.client.disco[key][None]['features'])))
+					register=QtGui.QPushButton("Register",self.ui.tree)
+					register.setMaximumWidth(40)
+					register.setMinimumWidth(40)
+					register.setEnabled(False)
+					register.jid=parentitem.text(3)
+					register.typ="register"
+					self.group.addButton(register)
+
+					self.ui.tree.setItemWidget(parentitem,2,register)
+					search=QtGui.QPushButton("Search",self.ui.tree)
+					search.setMaximumWidth(40)
+					search.setMinimumWidth(40)
+					search.setEnabled(False)
+					search.jid=parentitem.text(3)
+					search.typ="search"
+					self.group.addButton(search)
+					self.ui.tree.setItemWidget(parentitem,1,search)
+					if "jabber:iq:register" in list(self.main.client.disco[key][None]['features']):
+						register.setEnabled(True)
+					elif "jabber:iq:search" in list(self.main.client.disco[key][None]['features']):
+						search.setEnabled(True)
 				if self.main.client.disco[key][None].has_key("items"):
 					for item,values in self.main.client.disco[key][None]['items'].iteritems():
 						it=QtGui.QTreeWidgetItem(parentitem)
 						it.setText(0,item)
 						it.setIcon(0,parentitem.icon(0))
 						if values.has_key("jid"):
-							it.setText(1,values['jid'])
+							it.setText(3,values['jid'])
 				
 				#item=self.main.ui.bookmarks.findItems(jid,QtCore.Qt.MatchExactly,1)[0]
 			elif self.main.client.disco[key][None].has_key("err"):
@@ -130,7 +180,10 @@ class serviceDiscoveryDialog(QtGui.QDialog):
 		print categories.keys()
 		self.ui.tree.sortItems(0,QtCore.Qt.AscendingOrder)
 		self.ui.tree.resizeColumnToContents(0)
+		self.ui.tree.setColumnWidth (1,40)
+		self.ui.tree.setColumnWidth (2,40)
 		return categories
 
 	def accept(self):
 		self.done(1)
+
--- widgets/servicediscovery_ui.py
+++ widgets/servicediscovery_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'widgets/servicediscovery.ui'
 #
-# Created: Sun Oct 21 17:53:56 2007
+# Created: Tue Oct 23 08:34:32 2007
 #      by: PyQt4 UI code generator 4.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,31 +13,38 @@ from PyQt4 import QtCore, QtGui
 class Ui_serviceDiscovery(object):
     def setupUi(self, serviceDiscovery):
         serviceDiscovery.setObjectName("serviceDiscovery")
-        serviceDiscovery.resize(QtCore.QSize(QtCore.QRect(0,0,400,300).size()).expandedTo(serviceDiscovery.minimumSizeHint()))
+        serviceDiscovery.resize(QtCore.QSize(QtCore.QRect(0,0,717,298).size()).expandedTo(serviceDiscovery.minimumSizeHint()))
 
         self.gridlayout = QtGui.QGridLayout(serviceDiscovery)
         self.gridlayout.setMargin(9)
         self.gridlayout.setSpacing(6)
         self.gridlayout.setObjectName("gridlayout")
 
-        spacerItem = QtGui.QSpacerItem(291,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        spacerItem = QtGui.QSpacerItem(181,27,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
         self.gridlayout.addItem(spacerItem,1,0,1,1)
 
-        self.register = QtGui.QPushButton(serviceDiscovery)
-        self.register.setObjectName("register")
-        self.gridlayout.addWidget(self.register,1,1,1,1)
+        self.search = QtGui.QPushButton(serviceDiscovery)
+        self.search.setObjectName("search")
+        self.gridlayout.addWidget(self.search,1,1,1,1)
 
         self.tree = QtGui.QTreeWidget(serviceDiscovery)
         self.tree.setIconSize(QtCore.QSize(48,48))
         self.tree.setObjectName("tree")
-        self.gridlayout.addWidget(self.tree,0,0,1,2)
+        self.gridlayout.addWidget(self.tree,0,0,1,3)
+
+        self.register = QtGui.QPushButton(serviceDiscovery)
+        self.register.setObjectName("register")
+        self.gridlayout.addWidget(self.register,1,2,1,1)
 
         self.retranslateUi(serviceDiscovery)
         QtCore.QMetaObject.connectSlotsByName(serviceDiscovery)
 
     def retranslateUi(self, serviceDiscovery):
         serviceDiscovery.setWindowTitle(QtGui.QApplication.translate("serviceDiscovery", "Service Discovery", None, QtGui.QApplication.UnicodeUTF8))
-        self.register.setText(QtGui.QApplication.translate("serviceDiscovery", "Register", None, QtGui.QApplication.UnicodeUTF8))
+        self.search.setText(QtGui.QApplication.translate("serviceDiscovery", "Search", None, QtGui.QApplication.UnicodeUTF8))
         self.tree.headerItem().setText(0,QtGui.QApplication.translate("serviceDiscovery", "name", None, QtGui.QApplication.UnicodeUTF8))
-        self.tree.headerItem().setText(1,QtGui.QApplication.translate("serviceDiscovery", "jid", None, QtGui.QApplication.UnicodeUTF8))
+        self.tree.headerItem().setText(1,QtGui.QApplication.translate("serviceDiscovery", "search", None, QtGui.QApplication.UnicodeUTF8))
+        self.tree.headerItem().setText(2,QtGui.QApplication.translate("serviceDiscovery", "register", None, QtGui.QApplication.UnicodeUTF8))
+        self.tree.headerItem().setText(3,QtGui.QApplication.translate("serviceDiscovery", "jid", None, QtGui.QApplication.UnicodeUTF8))
+        self.register.setText(QtGui.QApplication.translate("serviceDiscovery", "Register", None, QtGui.QApplication.UnicodeUTF8))
 
