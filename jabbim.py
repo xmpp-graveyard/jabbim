bfc1d39dd13053805b33b7002d782ba5763e3c0b
hanz <hanz@users.noreply.github.com>
2007-10-30 19:11:04 +0000

    nastaveni xmllang

--- jabbim.py
+++ jabbim.py
@@ -1793,6 +1793,7 @@ class mainWindow(QtGui.QMainWindow):
 			else:
 				resource='jabbim'
 			self.client = clientClass(jid+"/"+resource, password, jid.split("@")[1], 5222,self,reactor)
+			self.client.xmlLang = unicode(QtCore.QLocale.system().name())[:2]
 			self.client.log=True
 		self.loadPlugins()
 		self.ui.login_connect.setEnabled(False)
