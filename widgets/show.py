8cdc963dff55a96fc09ec041e5a825000dab3505
kamahl <kamahl@users.noreply.github.com>
2007-11-03 18:08:34 +0000

    test escape

--- widgets/show.py
+++ widgets/show.py
@@ -1,4 +1,5 @@
 from PyQt4 import QtGui, QtCore
+from twisted.python import log
  
 class showLabel(QtGui.QLabel):
         def __init__(self, main, defaulttext = ""):
@@ -14,17 +15,30 @@ class showLabel(QtGui.QLabel):
 		self.main.lineEdit.setText(text)
 		self.main.lineEdit.show()
                 self.hide()
- 
+		
+class showLineEdit(QtGui.QLineEdit):
+	def __init__(self, label, parent):
+                QtGui.QLineEdit.__init__(self, parent)
+		self.label = label
+
+	def keyPressEvent(self, event):
+		key = event.key()
+		if key == QtCore.Qt.Key_Escape:
+			self.label.show()
+			self.hide()
+		else:
+			QtGui.QLineEdit.keyPressEvent(self, event)
+
 class showWidget(QtGui.QWidget):
 	def __init__(self, main, layout, labeltext = ""):
 		QtGui.QWidget.__init__(self, None)
 		self.main = main
 		self.label = showLabel(self, labeltext)
 		layout.addWidget(self.label)
-		self.lineEdit = QtGui.QLineEdit()
+		self.lineEdit = showLineEdit(self.label, self)
 		self.lineEdit.hide()
-		QtCore.QObject.connect(self.lineEdit,QtCore.SIGNAL("returnPressed ()"),self.lineEditToLabel)
 		QtCore.QObject.connect(self.lineEdit,QtCore.SIGNAL("lostFocus ()"),self.lineEditToLabel)
+		QtCore.QObject.connect(self.lineEdit,QtCore.SIGNAL("returnPressed ()"),self.lineEditToLabel)
                 layout.addWidget(self.lineEdit)
 		self.resize(300,50)
 		
@@ -39,4 +53,4 @@ class showWidget(QtGui.QWidget):
 			status = text,
 			show = self.main.client.roster['users'][self.main.client.jid.userhost()].resources[self.main.client.jid.resource].show
 		)
-		
+
