7f7b611bc2995771e6e47a2e1e03c4edec299c49
hanz <hanz@users.noreply.github.com>
2007-11-11 16:13:30 +0000

    zruseni adhoc pro kontakty bez resource

--- widgets/rosterLiveWidget.py
+++ widgets/rosterLiveWidget.py
@@ -2012,17 +2012,18 @@ class rosterWidget(QtGui.QWidget):
 				action = submenu.addAction(self.tr("Don't hide my status to contact"))
 				action.setData(QtCore.QVariant(jid))
 				action.setObjectName("privacy_unhide")
-		if oneres:
-			action=contactMenu.addAction(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
-			action.setData(QtCore.QVariant("%s/%s" % (jid, contact.resources.keys()[0])))
-			action.setObjectName("ad_hoc")
-		else:
-			submenu=contactMenu.addMenu(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
-			for res in contact.resources.keys():
-				if res != None:
-					action=submenu.addAction(res)
-					action.setData(QtCore.QVariant("%s/%s" %(jid,res)))
-					action.setObjectName("ad_hoc")
+		if len(contact.resources)!=0:
+			if oneres:
+				action=contactMenu.addAction(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
+				action.setData(QtCore.QVariant("%s/%s" % (jid, contact.resources.keys()[0])))
+				action.setObjectName("ad_hoc")
+			else:
+				submenu=contactMenu.addMenu(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
+				for res in contact.resources.keys():
+					if res != None:
+						action=submenu.addAction(res)
+						action.setData(QtCore.QVariant("%s/%s" %(jid,res)))
+						action.setObjectName("ad_hoc")
 		
 		# signal
 		contactMenu.connect(contactMenu, QtCore.SIGNAL("triggered ( QAction * )"),self.contactMenuTriggered)
