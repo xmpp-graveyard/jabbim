0f1287767b528762834d9bd60f9c702072c0a389
hanz <hanz@users.noreply.github.com>
2007-11-17 19:32:50 +0000

    cornelizace muc okna podle chat okna

--- widgets/groupchatwidget_ui.py
+++ widgets/groupchatwidget_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'widgets/groupchatwidget.ui'
 #
-# Created: Sat Nov 17 20:04:48 2007
+# Created: Sat Nov 17 20:32:57 2007
 #      by: PyQt4 UI code generator 4.3
 #
 # WARNING! All changes made in this file will be lost!
