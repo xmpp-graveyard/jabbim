c0fbcbb46e3ff9413e4297d2a929593de1d09183
hanz <hanz@users.noreply.github.com>
2007-11-22 17:17:53 +0000

    oprava skinu

--- jabbim.py
+++ jabbim.py
@@ -1881,7 +1881,7 @@ class mainWindow(QtGui.QMainWindow):
 			self.setStyleSheet(text)
 			self.xmlConsole.setStyleSheet(text)
 			self.chat.setStyleSheet(text)
-			self.ui.roster.theme=True
+			self.ui.roster.theme=False
 		self.ui.roster.reskin(text)
 
 
