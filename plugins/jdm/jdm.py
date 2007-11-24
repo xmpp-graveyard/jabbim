3e08e6dd963e03698adef8b763d7f74417563da5
josef.halicek <josef.halicek@users.noreply.github.com>
2007-11-24 22:10:10 +0000

    jdm zobrazuje ikony podle typu souboru, tzv. FileSorter (TM) :o)

--- plugins/jdm/jdm.py
+++ plugins/jdm/jdm.py
@@ -14,7 +14,7 @@ class Plugin(plugins.PluginBase):
 		self.description = 'Jabbim disk manager'
 		self.author = u"Josef 'Pepeq' Halíček"
 		self.name = 'JDM Plugin'
-		self.version = '0.1124'
+		self.version = '0.1131'
 		self.category = ['disk']
 		self.url = 'http://dev.jabbim.cz/jabbim'
 		if main:
@@ -30,16 +30,42 @@ class Plugin(plugins.PluginBase):
 			self.loadConfig(homedir)
 		
 	def updateView(self, vysledek, typ = 'public'):
+		self.window.ui.list.clear()
+		self.window.ui.log.clear()
+		self.obsah=[]
+		
 		#[0][0] je seznam jednotlivych polozek, kazda polozka ma nazev a velikost
 		for i in range (0,len(vysledek[0][0])):
-			self.obsah.append([i,vysledek[0][0][i][0],vysledek[0][0][i][0].split(".")[-1],vysledek[0][0][i][1]]);
+			self.obsah.append([i,vysledek[0][0][i][0],vysledek[0][0][i][0].split(".")[-1],vysledek[0][0][i][1]])
 			
 			#self.window.ui.log.append(u"Název: %s \nVelikost: %s bytů\ntywe :)\n"%(self.obsah[i][0],self.obsah[i][1]))
 
 		for i in range (0,len(self.obsah)):
 			self.window.ui.log.append(unicode(self.obsah[i]))
+			
 			item=QtGui.QListWidgetItem(unicode(self.obsah[i][1]))
-			item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/file.png"));
+			item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/text-x-generic-template.png"));  #preventivne pokud se netrefime
+			
+			if self.obsah[i][2] in ["exe","run","sh","bin"]: 
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/application-x-executable.png"))
+			if self.obsah[i][2] in ["svg","jpg","png","gif","tif","tiff","bmp","ico","xcf"]: 
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/image-x-generic.png"))
+			if self.obsah[i][2] in ["wav","mp3","ogg","mp4","flac"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/audio-x-generic.png"))
+			if self.obsah[i][2] in ["rar","zip","gz","bz","tgz","deb","rpm","tar","pkg","7z","ace"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/package-x-generic.png"))
+			if self.obsah[i][2] in ["htm","html","xml"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/text-html.png"))
+			if self.obsah[i][2] in ["txt","c","py","log"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/text-x-generic.png"))
+			if self.obsah[i][2] in ["mov","avi","mpg","swf","dv"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/text-x-generic.png"))
+			if self.obsah[i][2] in ["odt","doc","pdf","docx"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/x-office-document.png"))
+			if self.obsah[i][2] in ["ods","xls","cvs"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/x-office-spreadsheet.png"))
+			if self.obsah[i][2] in ["pts","ppt","odp"]:
+				item.setIcon(QtGui.QIcon(self.homeDir+"/plugins/jdm/x-office-presentation.png"))
 			self.window.ui.list.addItem(item)
 		
 		#for i in range (0,len(vysledek[0][0])):
