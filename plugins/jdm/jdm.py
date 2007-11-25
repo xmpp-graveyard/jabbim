c930467e7819418768f88d6fb990122954fc609a
josef.halicek <josef.halicek@users.noreply.github.com>
2007-11-25 20:16:23 +0000

    novy gui pro jdm

--- plugins/jdm/jdm.py
+++ plugins/jdm/jdm.py
@@ -14,7 +14,7 @@ class Plugin(plugins.PluginBase):
 		self.description = 'Jabbim disk manager'
 		self.author = u"Josef 'Pepeq' Halíček"
 		self.name = 'JDM Plugin'
-		self.version = '0.1131'
+		self.version = '0.1132'
 		self.category = ['disk']
 		self.url = 'http://dev.jabbim.cz/jabbim'
 		if main:
