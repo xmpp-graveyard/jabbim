af27378b0ed177643e7a173b6f9d6828b4abeb38
hanz <hanz@users.noreply.github.com>
2007-12-09 09:49:14 +0000

    test opravy spatneho pridavani kontaktu

--- pyxl/client.py
+++ pyxl/client.py
@@ -504,7 +504,7 @@ class Client(derived):
 
 	def addContact(self, jid, msg, name='', groups=[]):
 		log.msg( 'add contact')
-		self.sendPresence(to = params['jid'], status = params['msg'], typ = 'subscribe')
+		self.sendPresence(to = jid, status = msg, typ = 'subscribe')
 		#self.sendRosterUpdate(jid, name, 'none', groups, self._contactAdded, params = {'msg':msg, 'jid':jid})
 	
 	def _contactAdded(self, params):
