1cb8ae335d6f4343d446f1f3c1a33bc6aed71800
hanz <hanz@users.noreply.github.com>
2007-12-23 12:15:14 +0000

    autoDownload v preferencich

--- pref/jabbim.py
+++ pref/jabbim.py
@@ -2,4 +2,7 @@ class preferences:
 	def __init__(self,main):
 		self.main=main
 		self.config={}
-		self.config['saveGeometry']={'type':'boolean','label':self.main.tr("Save Jabbim position on close"),'value':'1'}
+		self.config['saveGeometry']={'type':'boolean','label':self.main.tr("Save Jabbim position on close"),'value':'True'}
+		self.config['autoDownload']={'type':'boolean','label':self.main.tr("Download incomming files automatically"),'value':'False','groupbox':self.main.tr('Incoming files')}
+		self.config['autoDownloadPath']={'type':'text-single','label':self.main.tr("Path for automatic download:"),'value':'','groupbox':self.main.tr('Incoming files')}
+		self.config['__sort__']=['saveGeometry','autoDownload','autoDownloadPath']
