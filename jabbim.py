a8e1eb40a125d2ea7806de9dd10f9c7b211b952c
hanz <hanz@users.noreply.github.com>
2007-12-27 03:46:54 +0000

    activated

--- jabbim.py
+++ jabbim.py
@@ -971,6 +971,8 @@ class clientClass(pyxl.client.Client):
 				tab.chat.textEditWrite(message)
 			else:
 				# add new chattab
+				if self.main.chat.isHidden():
+					self.main.chat.showMinimized()
 				self.main.chat.addChatTab(frm.full(),unicode(user),icon,message)
 				if len(body)>40:
 						traytext=body[:40]+" ..."
@@ -986,8 +988,7 @@ class clientClass(pyxl.client.Client):
 				text+="</td></tr></table>"
 				self.main.events.addInfoEvent(header=self.main.tr("New message"),text=self.main.tr("From: ")+unicode(user),name=unicode(frm.full()),typ='message',icon="images/xxxxx/actions/message.png",action=self.main.chat.activate,actionDict=[],tooltip=text)
 				self.main.tray.showMessage(self.main.tr("New message from ")+unicode(user), traytext, QtGui.QSystemTrayIcon.Information, 4000)
-				if self.main.chat.isHidden():
-					self.main.chat.showMinimized()
+
 		
 		# we found tab
 		if chatstate=="composing":
