1111e757cdfa443851e00e5f2694301a2d4ff4d5
hanz <hanz@users.noreply.github.com>
2007-12-30 20:06:21 +0000

    privacy fix

--- jabbim.py
+++ jabbim.py
@@ -72,16 +72,7 @@ class clientClass(pyxl.client.Client):
 #		log.msg("ERROR")
 #		QtGui.QMessageBox.warning(self.main,self.main.tr("Error"),unicode(fromjid+" "+code+" "+typ+" "+name),0,1)
 
-	def on_privacyReceived(self):
-		for item in self.privacy.active.items:
-			if item.value and item.typ == "jid":
-				for useritem in self.main.ui.roster.getUserItems(item.value):
-					useritem.privacy["block"] = self.privacy.active.isBlockedJID(item.value)
-					useritem.privacy["allow"] = self.privacy.active.isAllowedJID(item.value)
-					useritem.privacy["hide"] = self.privacy.active.isHiddenJID(item.value)
-
 	def on_privacyFail(self):
-		log.msg("privacy fail")
 		self.main.ui.actionPrivacy_list_editor.setEnabled(False)
 
 	def on_GCpresenceError(self, fromjid, code, typ, name, text, resource = ""):
@@ -345,8 +336,13 @@ class clientClass(pyxl.client.Client):
 								self.main.ui.roster.users.remove(i)
 		log.msg("METAITEMS:"+unicode(self.main.ui.roster.metaItems))
 
-		#if self.privacy != False:
-
+		if self.privacy != False:
+			for item in self.privacy.active.items:
+				if item.value and item.typ == "jid":
+					for useritem in self.main.ui.roster.getUserItems(item.value):
+						useritem.privacy["block"] = self.privacy.active.isBlockedJID(item.value)
+						useritem.privacy["allow"] = self.privacy.active.isAllowedJID(item.value)
+						useritem.privacy["hide"] = self.privacy.active.isHiddenJID(item.value)
 
 
 		#toDelJid=[] # contacts to delete
