30feb47516df4cbd5f8a8aca71947b6f95925702
sef <sef@users.noreply.github.com>
2007-12-30 13:46:13 +0000

    novy IPConfig [michich]

--- pyxl/IPConfig.py
+++ pyxl/IPConfig.py
@@ -1,3 +1,24 @@
+"""
+IPConfig - finds out about DNS servers configured in Windows
+Copyright (C) 2007  Michal "michich" Schmidt <mschmidt@redhat.com>
+
+The registry keys were found in James Macfarlane's Win32::IPConfig CPAN module.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 import _winreg
 
 class Adapter:
@@ -30,8 +51,6 @@ class IPConfig:
 				self.cards.append(card)
 		except EnvironmentError:
 			pass
-
-
 
 	def get_dns(self):
 		all_dns = []
