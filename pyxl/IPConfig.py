26cff6d20e0300b0ee704366488b9688a9c46f53
hanz <hanz@users.noreply.github.com>
2008-01-08 19:18:20 +0000

    IPConfig-debug.diff [michich]

--- pyxl/IPConfig.py
+++ pyxl/IPConfig.py
@@ -24,15 +24,22 @@ import _winreg
 class Adapter:
 	def __init__(self, interfaces_key, nic_uuid):
 		try:
+			print "Adapter: __init__"
 			tcpip_params_key = _winreg.OpenKey(interfaces_key, nic_uuid)
+			print "Adapter: key opened"
 			(dhcp_enabled, type) = _winreg.QueryValueEx(tcpip_params_key, "EnableDHCP")
+			print "Adapter: EnableDHCP=%s" % unicode(dhcp_enabled)
 			(nameserver, type) = _winreg.QueryValueEx(tcpip_params_key, "NameServer")
+			print "Adapter: NameServer=%s" % unicode(nameserver)
 			self.nameservers = nameserver.replace(',',' ').split(' ')
 
 			if nameserver == '' and dhcp_enabled:
+				print "Adapter: alternative branch"
 				(nameserver, type) = _winreg.QueryValueEx(tcpip_params_key, "DhcpNameServer")
+				print "Adapter: DhcpNameServer=%s" % unicode(nameserver)
 				self.nameservers = nameserver.split(' ')
 		except WindowsError:
+			print "Adapter: a key is not present"
 			self.nameservers = []
 	
 	def get_dns(self):
@@ -42,19 +49,25 @@ class IPConfig:
 	def __init__(self):
 		self.cards = []
 		hklm = _winreg.ConnectRegistry(None, _winreg.HKEY_LOCAL_MACHINE)
+		print "IPConfig: registry connected"
 		interfaces_key = _winreg.OpenKey(hklm, r'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces')
+		print "IPConfig: Interfaces key opened"
 		i = 0
 		try:
 			while True:
 				nic_uuid = _winreg.EnumKey(interfaces_key, i)
+				print "IPConfig: found uuid %s" % unicode(nic_uuid)
 				i = i + 1
 				card = Adapter(interfaces_key, nic_uuid)
 				self.cards.append(card)
+				print "IPConfig: adapter added"
 		except EnvironmentError:
+			print "IPConfig: no more uuids"
 			pass
 
 	def get_dns(self):
 		all_dns = []
 		for c in self.cards:
 			all_dns = all_dns + c.get_dns()
+		print "IPConfig: get_dns gives %s" % unicode(all_dns)
 		return all_dns
