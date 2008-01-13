96d866fe7b23af1a10fa70e2361dfbe1b730f845
hanz <hanz@users.noreply.github.com>
2008-01-13 13:48:42 +0000

    upravy paintEvent

--- widgets/rosterLiveWidget.py
+++ widgets/rosterLiveWidget.py
@@ -912,10 +912,10 @@ class rosterWidget(QtGui.QWidget):
 
 			painter.save()
 			painter.translate(x,y)
-				if self.theme:
-					painter.fillRect(0,0,self.width(),16,QtGui.QBrush(self.main.ui.userStyleWidget.palette().window()))
-				else:
-					painter.fillRect(0,0,self.width(),16,QtGui.QBrush(self.palet.color(QtGui.QPalette.Base)))
+			if self.theme:
+				painter.fillRect(0,0,self.width(),16,QtGui.QBrush(self.main.ui.userStyleWidget.palette().window()))
+			else:
+				painter.fillRect(0,0,self.width(),16,QtGui.QBrush(self.palet.color(QtGui.QPalette.Base)))
 			painter.restore()
 
 
