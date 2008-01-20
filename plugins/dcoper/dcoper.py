1461cde89aae60a6b5b9b9c0d6edadae9c0a3293
kamahl <kamahl@users.noreply.github.com>
2008-01-20 09:03:32 +0000

    nefunkční upgrade amarok pluginu

--- plugins/dcoper/dcoper.py
+++ plugins/dcoper/dcoper.py
@@ -7,6 +7,26 @@ from urllib import quote, unquote
 from twisted.python import log
 import commands
 
+class config:
+	def __init__(self, main):
+		self.main = main
+		self.config={}
+		self.config["format"] = {
+				"type": "text-multi",
+				"label": self.main.tr("Format of message. %artist is replaced by artist's name, %title by track name and so on for %album, %genre, %year, %type and %totalTime"),
+				"value": "/me hraje: (8) %artist - %title (%album) (8)"
+				}
+		self.config["amarok_off"] = {
+				"type": "text-single",
+				"label": self.main.tr("Message when amaroK is not running."),
+				"value": "/me má vypnutý amarok :'("
+				}
+		self.config["amarok_paused"] = {
+				"type": "text-single",
+				"label": self.main.tr("Message when nothing is being played in amaroK."),
+				"value": "/me zrovna nic nepřehrává :'("
+				}
+
 class Plugin(plugins.PluginBase):
 	def __init__(self,main, homedir):
 		plugins.PluginBase.__init__(self, main, homedir)
@@ -14,13 +34,22 @@ class Plugin(plugins.PluginBase):
 		self.description = 'DCOP plugin for /amarok'
 		self.author = "Josef 'Pepeq' Halicek & Jachym 'kamahl' Barvinek"
 		self.name = 'dcoper'
-		self.version = '0.001'
+		self.version = '0.1'
 		self.category = ['misc']
 		self.url = 'http://dev.jabbim.cz/jabbim'
 		self.count = 0
 		self.developMode=True
-		if main:
 
+		self.installTranslator()	
+
+		self.configDialog=config(self)
+
+		if main:
+			#jid = quote(unicode(self.main.client.jid.userhost()))
+			#if not os.path.isdir(self.main.homeDir+'/dcoper'):
+				#os.mkdir(self.main.homeDir+'/dcoper')
+			#if not os.path.isdir(self.main.homeDir+'/dcoper/'+ jid):
+				#os.mkdir(self.main.homeDir+'/dcoper/'+ jid)
 			self.registerHandler("onCommand", self.commandCalled, priority=5)
 			self.loadConfig()
 		else:
@@ -31,10 +60,21 @@ class Plugin(plugins.PluginBase):
 	def commandCalled(self, cmd, args, chat, typ):
 		if cmd != "amarok":
 			return
-		playing=unicode(commands.getoutput("dcop amarok player nowPlaying"), "utf-8")
-		if playing != "":
-			playing="/me hraje: (8) %s (8)" % playing
+		title = unicode(commands.getoutput("dcop amarok player title"))
+		if title == u"call failed":
+			msg = unicode(self.config["amarok_off"], "utf-8")
+		if title == u"":
+			msg = unicode(self.config["amarok_paused"], "utf-8")
 		else:
-			playing=unicode("AmaroK právě nic nepřehrává.", "utf-8")
-		self.main.client.sendMessage(unicode(chat.jid), playing, typ)
+			msg = unicode(self.config["format"], "utf-8")
+			msg = msg.replace("%title", title)
+			msg = msg.replace("%artist", commands.getoutput("dcop amarok player artist"))
+			msg = msg.replace("%album", commands.getoutput("dcop amarok player album"))
+			msg = msg.replace("%genre", commands.getoutput("dcop amarok player genre"))
+			msg = msg.replace("%year", commands.getoutput("dcop amarok player year"))
+			msg = msg.replace("%type", commands.getoutput("dcop amarok player type"))
+			msg = msg.replace("%totalTime", commands.getoutput("dcop amarok player totalTime"))
+		#playing=unicode(commands.getoutput("dcop amarok player nowPlaying"), "utf-8")
+		log.msg("Playing: "+`msg`)
+		self.main.client.sendMessage(unicode(chat.jid), msg, typ)
 
