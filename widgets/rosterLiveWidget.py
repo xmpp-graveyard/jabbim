ed61906eb1df1d2cefc59475ad1c98fdda6f9ab5
sef <sef@users.noreply.github.com>
2008-02-12 11:16:57 +0000

    oprava adhoc - contact menu

--- widgets/rosterLiveWidget.py
+++ widgets/rosterLiveWidget.py
@@ -2530,16 +2530,18 @@ class rosterWidget(QtGui.QWidget):
 					action.setObjectName("privacy_unhide")
 		if len(contact.resources)!=0:
 			if oneres:
-				action=contactMenu.addAction(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
-				action.setData(QtCore.QVariant("%s/%s" % (jid, contact.resources.keys()[0])))
-				action.setObjectName("ad_hoc")
+				if self.main.client.roster['users'][jid].resources[resource[0]].hasFeature('http://jabber.org/protocol/commands'):
+					action=contactMenu.addAction(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
+					action.setData(QtCore.QVariant("%s/%s" % (jid, contact.resources.keys()[0])))
+					action.setObjectName("ad_hoc")
 			else:
 				submenu=contactMenu.addMenu(QtGui.QIcon("images/16x16/actions/exec.png"),self.tr("Extra actions"))
 				for res in contact.resources.keys():
 					if res != None:
-						action=submenu.addAction(res)
-						action.setData(QtCore.QVariant("%s/%s" %(jid,res)))
-						action.setObjectName("ad_hoc")
+						if self.main.client.roster['users'][jid].resources[res].hasFeature('http://jabber.org/protocol/commands'):
+							action=submenu.addAction(res)
+							action.setData(QtCore.QVariant("%s/%s" %(jid,res)))
+							action.setObjectName("ad_hoc")
 				
 		
 		# signal
