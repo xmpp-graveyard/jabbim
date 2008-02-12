bb55575ba9331aacd4ad845d93848539993c4814
sef <sef@users.noreply.github.com>
2008-02-12 07:42:33 +0000

    fix pro #419

--- widgets/rosterLiveWidget.py
+++ widgets/rosterLiveWidget.py
@@ -828,7 +828,8 @@ class rosterWidget(QtGui.QWidget):
 						text+='<img src="images/16x16/status/jabber-%s.png">' % contact.resources[res].show # hodilo by se rozlisit k jakymu poatri transportu
 						if res != None:
 							text+='<b>%s</b> %s<br>' % (res, priority)
-						text+='<font size="-1">%s</font>' % (status)
+						if len(status) != 0:
+							text+='<font size="-1">%s</font><br />' % (status.replace('\n', '<br />'))
 					text+="</td></tr></table>"
 					self.setToolTip(text)
 		return QtGui.QWidget.event(self,event)
