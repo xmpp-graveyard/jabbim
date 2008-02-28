63681878d45edd6179a1245b8ab499f979f14da7
hanz <hanz@users.noreply.github.com>
2008-02-28 17:25:53 +0000

    updatovani tlacitka transportu podle presence

--- jabbim.py
+++ jabbim.py
@@ -660,6 +660,7 @@ class clientClass(pyxl.client.Client):
 			print "PRESENCE ERROR:"+unicode(error)
 			return
 		mainWindow=self.main
+		status=None
 
 		# get tab for this contact
 		tabFull,indexFull=self.main.chat.findTab(jid.full(),True,typ=['chat']) # tab with resource
@@ -744,6 +745,38 @@ class clientClass(pyxl.client.Client):
 					status=status.replace("\n"," ").replace("<","&lt;").replace(">","&gt;")
 				# update contact in roster
 				self.main.ui.roster.setStatus(jid,show,status=status,first=first)
+		if not isinstance(jid,unicode):
+			jid=jid.userhost()
+		# presence is from transport
+		if self.main.transports.has_key(jid):
+			if self.main.transports[jid]!=None:
+				self.main.transports[jid].setIcon(self.main.getIcon('1@'+jid,status=unicode(show),size="16x16"))
+
+				text='<table><tr>'
+				if os.path.isfile(self.main.homeDir+'/avatars/'+unicode(self.main.config['jid'])):
+					pixmap=QtGui.QIcon(self.main.homeDir+'/avatars/'+unicode(self.main.config['jid'])).pixmap(64,64)
+					text+='<td><img src="'+self.main.homeDir+'/avatars/'+unicode(self.main.config['jid'])+'" width="'+str(pixmap.width())+'" height="'+str(pixmap.height())+'"/></td>'
+				#text+='<td><b>'+self.main.tr("Name:")+'</b> '+item.escapedName+'<br/>'
+				text+='<td><b>'+self.main.tr("JID:")+'</b> '+unicode(self.main.config['jid'])+'<br/>'
+
+				#status = unicode(message)
+				#priority = pri
+				#if priority != None:
+					#priority = "(%s: %s)" % (self.main.tr("Priority"),priority)
+				#else:
+					#priority = ""
+				usertype=unicode(self.getHostType(jid,jid))
+				if os.path.isfile('images/16x16/status/'+usertype+"-"+show+".png"):
+					text+='<img src="images/16x16/status/'+usertype+"-"+show+'.png" />'
+				else:
+					text+='<img src="images/16x16/status/jabber-%s.png">' % show
+				#if len(priority)!=0:
+					#text+='%s<br/>' % priority
+				if status:
+					text+='<font size="-1">%s</font>' % (status)
+				text+="</td></tr></table>"
+				self.main.transports[jid].setToolTip(text)
+		
 
 	def on_xml(self,xml):
 		# append xml to the xml console, if it's enabled...
@@ -2004,34 +2037,34 @@ class mainWindow(QtGui.QMainWindow):
 					self.ui.statusMessage.setText(unicode(message))
 				self.ui.statusMessage.setIcon(self.getIcon(status=show,size="16x16"))
 		else:
-			# update transport's icon in statusWidgetMenu
-			if self.transports.has_key(jid):
-				if self.transports[jid]!=None:
-					self.transports[jid].setIcon(self.getIcon('1@'+jid,status=unicode(show),size="16x16"))
+			## update transport's icon in statusWidgetMenu
+			#if self.transports.has_key(jid):
+				#if self.transports[jid]!=None:
+					#self.transports[jid].setIcon(self.getIcon('1@'+jid,status=unicode(show),size="16x16"))
 
-					text='<table><tr>'
-					if os.path.isfile(self.homeDir+'/avatars/'+unicode(self.config['jid'])):
-						pixmap=QtGui.QIcon(self.homeDir+'/avatars/'+unicode(self.config['jid'])).pixmap(64,64)
-						text+='<td><img src="'+self.homeDir+'/avatars/'+unicode(self.config['jid'])+'" width="'+str(pixmap.width())+'" height="'+str(pixmap.height())+'"/></td>'
-					#text+='<td><b>'+self.tr("Name:")+'</b> '+item.escapedName+'<br/>'
-					text+='<td><b>'+self.tr("JID:")+'</b> '+unicode(self.config['jid'])+'<br/>'
+					#text='<table><tr>'
+					#if os.path.isfile(self.homeDir+'/avatars/'+unicode(self.config['jid'])):
+						#pixmap=QtGui.QIcon(self.homeDir+'/avatars/'+unicode(self.config['jid'])).pixmap(64,64)
+						#text+='<td><img src="'+self.homeDir+'/avatars/'+unicode(self.config['jid'])+'" width="'+str(pixmap.width())+'" height="'+str(pixmap.height())+'"/></td>'
+					##text+='<td><b>'+self.tr("Name:")+'</b> '+item.escapedName+'<br/>'
+					#text+='<td><b>'+self.tr("JID:")+'</b> '+unicode(self.config['jid'])+'<br/>'
 
-					status = unicode(message)
-					priority = pri
-					if priority != None:
-						priority = "(%s: %s)" % (self.tr("Priority"),priority)
-					else:
-						priority = ""
-					usertype=unicode(self.client.getHostType(jid,jid))
-					if os.path.isfile('images/16x16/status/'+usertype+"-"+show+".png"):
-						text+='<img src="images/16x16/status/'+usertype+"-"+show+'.png" />'
-					else:
-						text+='<img src="images/16x16/status/jabber-%s.png">' % show
-					if len(priority)!=0:
-						text+='%s<br/>' % priority
-					text+='<font size="-1">%s</font>' % (status)
-					text+="</td></tr></table>"
-					self.transports[jid].setToolTip(text)
+					#status = unicode(message)
+					#priority = pri
+					#if priority != None:
+						#priority = "(%s: %s)" % (self.tr("Priority"),priority)
+					#else:
+						#priority = ""
+					#usertype=unicode(self.client.getHostType(jid,jid))
+					#if os.path.isfile('images/16x16/status/'+usertype+"-"+show+".png"):
+						#text+='<img src="images/16x16/status/'+usertype+"-"+show+'.png" />'
+					#else:
+						#text+='<img src="images/16x16/status/jabber-%s.png">' % show
+					#if len(priority)!=0:
+						#text+='%s<br/>' % priority
+					#text+='<font size="-1">%s</font>' % (status)
+					#text+="</td></tr></table>"
+					#self.transports[jid].setToolTip(text)
 
 			# send presence
 			self.client.sendPresence(to=jid,show = unicode(show), status = unicode(message),priority=pri)
