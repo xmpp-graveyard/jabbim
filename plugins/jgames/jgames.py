c9747eca0e067464ac223474fdb0998b1f6709a8
hanz <hanz@users.noreply.github.com>
2008-03-11 13:08:54 +0000

    victory

--- plugins/jgames/jgames.py
+++ plugins/jgames/jgames.py
@@ -198,6 +198,7 @@ class gameObj:
 		#2008/03/11 14:44 +0200 [-] {u'type': u'victory', u'value': u'pyjim@jabber.cz/jabbimSvn'}
 		#2008/03/11 14:44 +0200 [-] ['__class__', '__cmp__', '__contains__', '__delattr__', '__delitem__', '__doc__', '__eq__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__init__', '__iter__', '__le__', '__len__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setitem__', '__str__', 'clear', 'copy', 'fromkeys', 'get', 'has_key', 'items', 'iteritems', 'iterkeys', 'itervalues', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values']
 		self.dialog.state=[attr['type'],attr['value']]
+		self.dialog.repaint()
 	
 	
 
