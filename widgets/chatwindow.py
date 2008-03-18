824d41a67af8bf73f5ee6c01613dff8b877a9b49
hanz <hanz@users.noreply.github.com>
2008-03-18 21:57:49 +0000

    oprava avataru

--- widgets/chatwindow.py
+++ widgets/chatwindow.py
@@ -605,9 +605,9 @@ class chatWindow(QtGui.QMainWindow):
 		file = None
 		#print self.main.client.avatars
 		if self.main.client.avatarDef.has_key(w.jid+'/'+user):
-			file = self.main.realHomeDir+'/avatars/'+self.main.client.avatarDef[w.jid+'/'+user]
+			file = self.main.realHomeDir+'/avatars/'+str(self.main.client.avatarDef[w.jid+'/'+user])
 		elif truejid != None and self.main.client.avatarDef.has_key(truejid):
-			file = self.main.realHomeDir+'/avatars/'+self.main.client.avatarDef[truejid]
+			file = self.main.realHomeDir+'/avatars/'+str(self.main.client.avatarDef[truejid])
 		else:
 			#self.getVCard(frm+'/'+user) #tohle asi neni potreba
 			pass
