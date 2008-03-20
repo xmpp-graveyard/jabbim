7ca6cc0846b9933906bb7aeaab1d9be12d98730e
sef <sef@users.noreply.github.com>
2008-03-20 11:34:34 +0000

    opravy v EasyShare

--- plugins/easyshare/easyshare.py
+++ plugins/easyshare/easyshare.py
@@ -25,7 +25,7 @@ from twisted.python import log
 class ResendFile(Stage):
 	def exec_(self):
 		self.status = "executing"
-		self.actions = {"cancel":CancelStage, "next":ResendFile, "execute":ResendFile}
+		self.actions = {"cancel":CancelStage, "next":ResendFile, 'prev': ResendFile, "execute":ResendFile}
 		self.execute = "next"
 
 		print self.session.args
@@ -34,9 +34,16 @@ class ResendFile(Stage):
 
 		print self.data
 
-		if self.data == None:
-			pwd = self.session.args['home']
-			self.session.args['last'] = pwd
+		if self.data == None or len(self.data['file']) == 0:
+			if not self.session.args.has_key('last'):
+				print 'nemam last'
+				pwd = self.session.args['home']
+				self.session.args['last'] = pwd
+			else:
+				if self.session.args['last'] != self.session.args['home']:
+					pwd = os.path.split(self.session.args['last'])[0]
+				else:
+					pwd = self.session.args['home']
 		
 		else:
 
@@ -48,28 +55,28 @@ class ResendFile(Stage):
 				elif os.path.isfile(os.path.join(self.session.args['last'], file)):
 					files.append(file)
 		
-		print dirs, files
-		
-		if len(files)>0:
-			desc = {}
-			fajly = []
-			for f in files:
-				ff = os.path.join(self.session.args['last'], f)
-				desc[ff] = 'EasyShare'
-				fajly.append(ff)
-			self.main.events.addFTUploadEvent(self.session.jid, fajly, desc)
-			self.status = "completed"
-			self.execute = None
-			self.actions={}
-			self.xform = None
-			return
-		elif len(dirs)>0:
-			if dirs[0] == os.path.pardir:
-				pwd = os.path.split(self.session.args['last'])[0]
+			print dirs, files
+			
+			if len(files)>0:
+				desc = {}
+				fajly = []
+				for f in files:
+					ff = os.path.join(self.session.args['last'], f)
+					desc[ff] = 'EasyShare'
+					fajly.append(ff)
+				self.main.events.addFTUploadEvent(self.session.jid, fajly, desc)
+				self.status = "completed"
+				self.execute = None
+				self.actions={}
+				self.xform = None
+				return
+			elif len(dirs)>0:
+				if dirs[0] == os.path.pardir:
+					pwd = os.path.split(self.session.args['last'])[0]
+				else:
+					pwd = os.path.join(self.session.args['last'], dirs[0])
 			else:
-				pwd = os.path.join(self.session.args['last'], dirs[0])
-		else:
-			pwd = self.session.args['home']
+				pwd = self.session.args['home']
 					
 #		elif os.path.isdir(os.path.join(self.data["pwd"][0], self.data["file"][0])):
 #			if self.data["file"][0] == os.path.pardir:
@@ -83,11 +90,14 @@ class ResendFile(Stage):
 		print pwd, pwd ==  self.session.args['home']
 		print self.session.args['home']
 		for f in os.listdir(unicode(pwd)):
-			if os.access(os.path.join(pwd,f), os.R_OK):
-				if os.path.isfile(os.path.join(pwd,f)):
-					files.append([f, f])
-				else:
-					dirs.append(["%s%s" % (f, os.path.sep), f])
+			try:
+				if os.access(os.path.join(pwd,f), os.R_OK):
+					if os.path.isfile(os.path.join(pwd,f)):
+						files.append([f, f])
+					else:
+						dirs.append(["%s%s" % (f, os.path.sep), f])
+			except:
+				print 'chyba v listovani'
 		files.sort()
 		dirs.sort()
 		if pwd != self.session.args['home']:
@@ -143,6 +153,9 @@ class Plugin(plugins.PluginBase):
 				if jd.strip() != '':
 					self.public.append(jd.strip())
 			self.main.client.commands.registerNode("http://dev.jabbim.cz/jabbim/rc#easyshare", "Get file", ResendFile, public = self.public, args = {'home':self.home})
+	
+	def on_remove(self):
+		self.main.client.commands.unregisterNode("http://dev.jabbim.cz/jabbim/rc#easyshare", "Get file")
 	
 	def buildContactMenu(self, menu, contact):
 		self.action=menu.addAction(self.tr("EasyShare"))
