6d34ff92edb233d79a730b90fb7cd1f43844cdd0
hanz <hanz@users.noreply.github.com>
2008-04-20 19:19:44 +0000

    oprava spatneho body v groupchatMessageForMeEvent a groupchatMessageEvent

--- widgets/chatwindow.py
+++ widgets/chatwindow.py
@@ -639,10 +639,10 @@ class chatWindow(QtGui.QMainWindow):
 						if self.main.skin.has_key('my_message_continue'):
 							message=self.main.skin["my_message_continue"].replace("[time]",self.main.now()).replace("[user]",user)
 			else:
-				self.main.client.dispatcher.publishEvent('groupchatMessageEvent', self.main.getJid(w.jid),user,body,subject, xhtml)
+				self.main.client.dispatcher.publishEvent('groupchatMessageEvent', self.main.getJid(w.jid),user,oldbody,subject, xhtml)
 				# it's message for us
 				if utils.need_highlight(unicode(w.chat.nick), unicode(oldbody)) and not unicode(body).startswith("/me"):
-					self.main.client.dispatcher.publishEvent('groupchatMessageForMeEvent', self.main.getJid(w.jid),user,body,subject, xhtml)
+					self.main.client.dispatcher.publishEvent('groupchatMessageForMeEvent', self.main.getJid(w.jid),user,oldbody,subject, xhtml)
 					if int(self.ui.chatTab.currentIndex())!=i:
 						if self.ui.chatTab.tabBar().tabTextColor(i).name()!=QtGui.QColor(255,0,0).name():
 							self.ui.chatTab.setTabIcon(i,QtGui.QIcon("images/16x16/actions/message.png"))
