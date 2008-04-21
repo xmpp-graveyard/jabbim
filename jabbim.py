52f46a2107615338e1c951ce66c99a27091db699
hanz <hanz@users.noreply.github.com>
2008-04-21 18:10:41 +0000

    oprava loadSkin

--- jabbim.py
+++ jabbim.py
@@ -3356,6 +3356,7 @@ class mainWindow(QtGui.QMainWindow):
 		Loads chat skin. Skin is loaded to self.skin.
 		"""
 		loaded,self.skin=self.loadJabbimExtraConfig("chatskins/"+self.config['chatSkin'],self.realHomeDir+"/chatskins/"+self.config['chatSkin'])
+		self.skin=self.skin['chatskin']
 		if not self.skin.has_key("spaces_between_lines"):
 			self.skin["spaces_between_lines"]='0'
 	
