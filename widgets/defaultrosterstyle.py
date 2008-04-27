df0099d10f6158739920682b575b82fcf72470ca
hanz <hanz@users.noreply.github.com>
2008-04-27 14:13:27 +0000

    hotfix pro roster

---
+++ widgets/defaultrosterstyle.py
@@ -0,0 +1,279 @@
+import os
+try:
+	from PyQt4 import QtCore, QtGui
+except:
+	print "PyQt4 is not installed."
+
+class rosterStyle:
+	def __init__(self,roster):
+		self.roster=roster
+
+	def heightForItem(self,item):
+		#doc=QtGui.QTextDocument()
+		#doc.setHtml("<font size=\"-1\">font test</font>")
+		#cursor=doc.find("test")
+		#metrics=QtGui.QFontMetrics(cursor.charFormat().font())
+		#doc.setPageSize(QtCore.QSizeF(0,0,self.roster.width()-64),metrics.height())
+		#height=
+
+		##rect=metrics.boundingRect(0, 0,self.roster.width()-64, self.roster.height(), QtCore.Qt.TextWordWrap, unicode(item.statusMessage))
+		##height=rect.height()
+		#print height,metrics.height(),QtGui.QApplication.fontMetrics().height(),cursor.isNull()
+		#if height-16>=0:
+			#return 32+height-16
+		#else:
+		return 32
+
+	def paintSelectedUserItem(self,painter,useritem,x,y):
+		height=32
+		self.roster.selectedHeight=28
+
+		# set pen and brush for item background
+		b=painter.brush()
+		p=painter.pen()
+		if self.roster.theme:
+			painter.setBrush(self.roster.main.ui.selectedItemStyle.palette().window())
+			pen=QtGui.QPen(self.roster.main.ui.selectedItemStyle.palette().color(QtGui.QPalette.Text))
+		else:
+			painter.setBrush(self.roster.main.ui.selectedItemStyle.palette().color(QtGui.QPalette.Highlight))
+			color=self.roster.main.ui.selectedItemStyle.palette().color(QtGui.QPalette.Highlight)
+			# Qt4.2 uses .light() but Qt4.3 uses lighter(), so we have to try both of them because of compatibility
+			try:
+				pen=QtGui.QPen(color.lighter())
+			except:
+				pen=QtGui.QPen(color.light())
+		pen.setWidth(0)
+		painter.setPen(pen)
+
+		# paint item background and border
+		painter.save()
+		painter.translate(x,y-2)
+		painter.drawRect(1,0,self.roster.width()-2,height+2)
+		painter.restore()
+		painter.setBrush(b)
+		painter.setPen(p)
+
+		## events... TODO: this have to be rewrited in some better way
+		#if useritem in self.roster.events:
+			#if self.roster.bl:
+				#painter.drawPixmap(x+7,y,useritem.icon.pixmap(32,32))
+			#else:
+				#painter.drawPixmap(x+7,y,useritem.blink.pixmap(32,32))
+		#else:
+			#if useritem.privacy['block'] or useritem.privacy['hide']:
+				#painter.drawPixmap(x+7,y,self.roster.main.getIcon(status="error",size="32x32").pixmap(32,32))
+			#else:
+				#painter.drawPixmap(x+7,y,useritem.icon.pixmap(32,32))
+		
+		# get font height
+		doc=QtGui.QTextDocument()
+		font=QtGui.QApplication.fontMetrics()
+		fontHeight=int(font.height())
+		
+		# show resources count
+		res=""
+		if len(self.roster.main.client.roster['users'][useritem.jid].resources)>1:
+			res=" ("+str(len(self.roster.main.client.roster['users'][useritem.jid].resources))+")"
+
+		# get avatar
+		avatar=self.roster.main.client.getAvatarImg(useritem.jid)
+		if avatar:
+			avatar=avatar[0]
+		else:
+			avatar=self.roster.main.client.getAvatarImg(None)
+			if avatar:
+				avatar=avatar[0]
+		
+		# get foreground font color
+		if self.roster.theme:
+			fontColor=self.roster.main.ui.userStyleWidget.palette().color(QtGui.QPalette.Text).name()
+		else:
+			fontColor=self.roster.palet.color(QtGui.QPalette.HighlightedText).name()
+
+		if useritem.statusMessage:
+			# show contact name
+			doc.setHtml("<font color=\""+fontColor+"\">"+useritem.escapedName+res+"</font>")
+			painter.save()
+			painter.translate(x+41,y+2)
+			if avatar:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-43-32,y+32))
+			else:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-41,y+32))
+			painter.restore()
+			# show contact status message
+			doc.setHtml("<font size=\"-1\" color=\""+fontColor+"\"><i>"+useritem.statusMessage+"</i></font>")
+			painter.save()
+			painter.translate(x+41,y+16)
+			if avatar:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-43-32,y+32))
+			else:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-41,y+32))
+			painter.restore()
+		else:
+			doc.setHtml("<font color=\""+fontColor+"\">"+useritem.escapedName+res+"</font>")
+			painter.save()
+			painter.translate(x+41,y+(32-fontHeight)/2)
+			if useritem.avatar:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-38-32,y+28))
+			else:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-38,y+28))
+			painter.restore()
+		if avatar:
+			painter.drawPixmap(x+7,y,avatar)
+			if useritem.mood:
+				painter.drawPixmap(self.roster.width()-4-16,y,useritem.mood)
+			if useritem.tune:
+				painter.drawPixmap(self.roster.width()-4-16,y+16,useritem.tune)
+
+		if useritem in self.roster.events:
+			if self.roster.bl:
+				painter.drawPixmap(x+7+16,y+16,useritem.icon.pixmap(16,16))
+			else:
+				painter.drawPixmap(x+7+16,y+16,useritem.blink.pixmap(16,16))
+		else:
+			if useritem.privacy['block'] or useritem.privacy['hide']:
+				painter.drawPixmap(x+7+16,y+16,self.roster.main.getIcon(status="error",size="16x16").pixmap(16,16))
+			else:
+				painter.drawPixmap(x+7+16,y+16,useritem.icon.pixmap(16,16))
+	
+	def paintUnselectedUserItem(self,painter,useritem,x,y):
+		painter.save()
+		painter.translate(x,y)
+		background=None
+		if useritem==self.roster.selected:
+			if self.roster.theme:
+				painter.fillRect(0,0,self.roster.width(),32,QtGui.QBrush(self.roster.main.ui.selectedItemStyle.palette().window()))
+			else:
+				painter.fillRect(0,0,self.roster.width(),32,QtGui.QBrush(self.roster.main.ui.selectedItemStyle.palette().color(QtGui.QPalette.Highlight)))
+		else:
+			if self.roster.theme:
+				brush=QtGui.QBrush(self.roster.main.ui.userStyleWidget.palette().window())
+				if brush.color().alpha()!=0:
+					painter.fillRect(0,0,self.roster.width(),32,brush)
+				background=brush.color()
+				#painter.fillRect(0,0,self.roster.width(),32,QtGui.QBrush(self.roster.main.ui.userStyleWidget.palette().window()))
+			else:
+				painter.fillRect(0,0,self.roster.width(),32,QtGui.QBrush(self.roster.palet.color(QtGui.QPalette.Base)))
+				background=self.roster.palet.color(QtGui.QPalette.Base)
+		painter.restore()
+		if self.roster.metaItems.has_key(useritem.metajid) and background:
+			if not useritem in self.roster.metaItems[useritem.metajid]:
+				painter.save()
+				painter.translate(x,y)
+				painter.setPen(QtCore.Qt.transparent)
+				linearGrad=QtGui.QRadialGradient(QtCore.QPointF(2,16),14)
+				if self.roster.theme:
+					linearGrad.setColorAt(0,self.roster.main.ui.selectedItemStyle.palette().window().color())
+				else:
+					linearGrad.setColorAt(0,self.roster.main.ui.selectedItemStyle.palette().color(QtGui.QPalette.Highlight))
+				linearGrad.setColorAt(1, background)
+				painter.setBrush(linearGrad)
+				painter.drawRect(0,0,15,32)
+				if self.roster.theme:
+					painter.setPen(QtGui.QPen(self.roster.main.ui.selectedItemStyle.palette().text().color()))
+				else:
+					painter.setPen(QtGui.QPen(self.roster.main.ui.selectedItemStyle.palette().color(QtGui.QPalette.HighlightedText)))
+				if useritem.expanded:
+					painter.drawPoint(2,15)# 00000
+					painter.drawPoint(3,15)#  000
+					painter.drawPoint(4,15)#   0
+					painter.drawPoint(5,15)
+					painter.drawPoint(6,15)
+					painter.drawPoint(3,16)
+					painter.drawPoint(4,16)
+					painter.drawPoint(5,16)
+					painter.drawPoint(4,17)
+				else:
+					painter.drawPoint(5,16)
+					painter.drawPoint(4,15)
+					painter.drawPoint(4,16)
+					painter.drawPoint(4,17)
+					painter.drawPoint(3,14)
+					painter.drawPoint(3,15)
+					painter.drawPoint(3,16)
+					painter.drawPoint(3,17)
+					painter.drawPoint(3,18)
+				painter.restore()
+			
+		doc=QtGui.QTextDocument()
+		font=QtGui.QApplication.fontMetrics()
+		fontHeight=int(font.height())
+
+		res=""
+		if len(self.roster.main.client.roster['users'][useritem.jid].resources)>1:
+			res=" ("+str(len(self.roster.main.client.roster['users'][useritem.jid].resources))+")"
+		
+		avatar=self.roster.main.client.getAvatarImg(useritem.jid)
+		if avatar:
+			avatar=avatar[0]
+		else:
+			avatar=self.roster.main.client.getAvatarImg(None)
+			if avatar:
+				avatar=avatar[0]
+
+		if useritem==self.roster.selected:
+			if self.roster.theme:
+				fontcolor=self.roster.main.ui.userStyleWidget.palette().color(QtGui.QPalette.Text).name()
+			else:
+				fontcolor=self.roster.palet.color(QtGui.QPalette.HighlightedText).name()
+		else:
+			fontcolor=self.roster.main.ui.userStyleWidget.palette().color(QtGui.QPalette.Text).name()
+		if useritem.statusMessage:
+			doc.setHtml("<font color=\""+fontcolor+"\">"+useritem.escapedName+res+"</font>")
+			painter.save()
+			painter.translate(x+41,y+2)
+			if avatar:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-43-32,y+32))
+			else:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-41,y+32))
+			painter.restore()
+			doc.setHtml("<font size=\"-1\" color=\""+fontcolor+"\"><i>"+useritem.statusMessage+"</i></font>")
+			painter.save()
+			painter.translate(x+41,y+16)
+			#if avatar:
+				#doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-43-32,y+32))
+			#else:
+			opt=doc.defaultTextOption()
+			opt.setWrapMode(QtGui.QTextOption.WrapAtWordBoundaryOrAnywhere)
+			doc.setDefaultTextOption(opt)
+			doc.setPageSize(QtCore.QSizeF(self.roster.width()-41,useritem.height-16))
+			doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-41,y+32))
+			painter.restore()
+		else:
+			doc.setHtml("<font color=\""+fontcolor+"\">"+useritem.escapedName+res+"</font>")
+			painter.save()
+			painter.translate(x+41,y+(32-fontHeight)/2)
+			if avatar:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-38-32,y+28))
+			else:
+				doc.drawContents(painter, QtCore.QRectF(0,0,self.roster.width()-38,y+28))
+			painter.restore()
+		if avatar:
+			painter.drawPixmap(x+7,y,avatar)
+			if useritem.mood:
+				painter.drawPixmap(self.roster.width()-4-16,y,useritem.mood)
+			if useritem.tune:
+				painter.drawPixmap(self.roster.width()-4-16,y+16,useritem.tune)
+
+		if useritem in self.roster.events:
+			if self.roster.bl:
+				painter.drawPixmap(x+7+16,y+16,useritem.icon.pixmap(16,16))
+			else:
+				painter.drawPixmap(x+7+16,y+16,useritem.blink.pixmap(16,16))
+		else:
+			if useritem.privacy['block'] or useritem.privacy['hide']:
+				painter.drawPixmap(x+7+16,y+16,self.roster.main.getIcon(status="error",size="16x16").pixmap(16,16))
+			else:
+				painter.drawPixmap(x+7+16,y+16,useritem.icon.pixmap(16,16))
+
+
+	def paintUserItem(self,painter,useritem,x,y):
+		"""
+		paints user item in normal roster
+		"""
+		if useritem==self.roster.item:
+			self.paintSelectedUserItem(painter,useritem,x,y)
+		else:
+			self.paintUnselectedUserItem(painter,useritem,x,y)
+	
+
