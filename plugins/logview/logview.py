77d3db362be2cd031719231a477ffd4db47e08b0
sef <sef@users.noreply.github.com>
2008-06-02 20:43:17 +0000

    opraseny logview plugin

--- plugins/logview/logview.py
+++ plugins/logview/logview.py
@@ -11,38 +11,45 @@ class Plugin(plugins.PluginBase):
 		self.description = 'Extra debug window'
 		self.author = "Jiri 'Sef' Gabrys"
 		self.name = 'LogView Plugin'
-		self.version = '0.033'
+		self.version = '0.04'
 		self.category = ['log', 'misc']
 		self.url = 'http://dev.jabbim.cz/jabbim'
 		#self.config['notify'] = {'description':'', 'default':'True', 'value': '','type':'boolean'}
 		if main:
 			self.loadConfig()
-			#self.window = self.loadWindow("%s/logWindow.ui.py" % self.pluginDir)
-			#self.window.setWindowIcon(self.main.windowIcon())
-			#self.log = False
-			#QtCore.QObject.connect(self.window.ui.enableBox, QtCore.SIGNAL("stateChanged(int)"),self.enableToggled)
-			#QtCore.QObject.connect(self.window.ui.clearButton, QtCore.SIGNAL("clicked()"),self.clearLog)
-			
-	#def buildRosterMenu(self):
-		#menu=self.rosterMenu()
-		#menu.addAction("Show log",self.showSlot)
+			self.window = self.loadWindow("%s/logWindow.ui.py" % self.pluginDir)
+			self.window.setWindowIcon(self.main.windowIcon())
+			self.log = False
+			QtCore.QObject.connect(self.window.ui.enableBox, QtCore.SIGNAL("stateChanged(int)"),self.enableToggled)
+			QtCore.QObject.connect(self.window.ui.clearButton, QtCore.SIGNAL("clicked()"),self.clearLog)
+		else:
+			self.loadConfig(homedir)
 	
-	#def showSlot(self):
-		#self.window.show()
 	
-	#def enableToggled(self, state):
-		#if (not self.log) and self.window.ui.enableBox.isChecked():
-			#self.log = True
-			#log.addObserver(self.observer)
-## 			self.window.ui.logView.append('Log started')
-		#else:
-			#self.log = False
-			#log.removeObserver(self.observer)
 	
-	#def observer(self, msg):
-		#self.window.ui.logView.append('[%s] %s' %(time.strftime('%X'), unicode(' '.join(msg['message']))))
-		#if msg['isError']:
-			#self.main.tray.showMessage(self.main.tr("Log"),unicode(' '.join(msg['message'])), QtGui.QSystemTrayIcon.Warning, 2000)
+	def buildMainWindowMenu(self):
+		menu=self.mainWindowMenu()
+		menu.addAction("Show log",self.showSlot)
 	
-	#def clearLog(self):
-		#self.window.ui.logView.setText('')
+	def on_remove(self):
+		log.removeObserver(self.observer)
+	
+	def showSlot(self):
+		self.window.show()
+	
+	def enableToggled(self, state):
+		if (not self.log) and self.window.ui.enableBox.isChecked():
+			self.log = True
+			log.addObserver(self.observer)
+			self.window.ui.logView.append('Log started')
+		else:
+			self.log = False
+			log.removeObserver(self.observer)
+	
+	def observer(self, msg):
+		self.window.ui.logView.append('[%s] %s' %(time.strftime('%X'), unicode(' '.join(msg['message']))))
+		if msg['isError']:
+			self.main.tray.showMessage(self.main.tr("Log"),unicode(' '.join(msg['message'])), QtGui.QSystemTrayIcon.Warning, 2000)
+	
+	def clearLog(self):
+		self.window.ui.logView.setText('')
