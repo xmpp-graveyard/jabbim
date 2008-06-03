2697da4fceba1b9ab64530a5fc39f1566176836d
hanz <hanz@users.noreply.github.com>
2008-06-03 17:17:05 +0000

    opravy unicode

--- widgets/chatwidget.py
+++ widgets/chatwidget.py
@@ -354,8 +354,8 @@ class chatWidget(abstractChatWidget):
 			text+='<br />%s <font size="-1">&nbsp; %s</font>' % (icon,t)
 			if self.main.config['showMoodChanges']=='True':
 				user=unicode(self.main.ui.roster.getNameByJID(self.jid))
-				message=icon+"&nbsp;"+user+" "+self.tr("is now")+" "+ t
-				self.textEditWrite(self.main.skin["status_message"].replace("[time]",self.main.now()).replace('[message]',message))
+				message=icon+"&nbsp;"+user+" "+unicode(self.tr("is now"))+" "+ t
+				self.textEditWrite(self.main.skin["status_message"].replace("[time]",self.main.now()).replace('[message]',unicode(message)))
 		tune = contact.getPEP('http://jabber.org/protocol/tune')
 		if type(tune) == list:
 			for x in tune:
@@ -372,8 +372,8 @@ class chatWidget(abstractChatWidget):
 				text+='<br /><img src="images/22x22/icons/headphones.png" /><font size="-1">&nbsp; %s</font>' % (t) #ikonka se este muze menit ;)
 				if self.main.config['showTuneChanges']=='True':
 					user=unicode(self.main.ui.roster.getNameByJID(self.jid))
-					message='<img src="images/22x22/icons/headphones.png" />&nbsp;'+user+" "+self.tr("is now listening:")+" "+icon+" "+ t
-					self.textEditWrite(self.main.skin["status_message"].replace("[time]",self.main.now()).replace('[message]',message))
+					message='<img src="images/22x22/icons/headphones.png" />&nbsp;'+user+" "+unicode(self.tr("is now listening:"))+" "+icon+" "+ t
+					self.textEditWrite(self.main.skin["status_message"].replace("[time]",self.main.now()).replace('[message]',unicode(message)))
 
 		self.ui.label.setText(text)
 
