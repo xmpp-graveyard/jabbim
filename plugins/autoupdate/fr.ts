adb2718bfb79192996e91c004f771a168340c395
hanz <hanz@users.noreply.github.com>
2008-06-10 14:12:45 +0000

    czech translation for autoupdate plugin

---
+++ plugins/autoupdate/fr.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="autoupdate.py" line="46"/>
+        <source>Plugin updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="autoupdate.py" line="46"/>
+        <source>Please restart Jabbim to apply changes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="autoupdate.py" line="73"/>
+        <source>Autoupdate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="autoupdate.py" line="65"/>
+        <source>New version of plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="autoupdate.py" line="65"/>
+        <source>is available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="autoupdate.py" line="66"/>
+        <source>Do you want to update this plugin?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="autoupdate.py" line="73"/>
+        <source>New version of Jabbim is available!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>self.main</name>
+    <message>
+        <location filename="autoupdate.py" line="66"/>
+        <source>update</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
