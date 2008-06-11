7bcc61a741e3fc8c35e2f183b96857b3270f9c6f
hanz <hanz@users.noreply.github.com>
2008-06-11 13:15:53 +0000

    easyshare de la france

--- plugins/easyshare/fr.ts
+++ plugins/easyshare/fr.ts
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="fr_FR">
+<defaultcodec></defaultcodec>
 <context>
     <name>Plugin</name>
     <message>
         <location filename="easyshare.py" line="167"/>
         <source>EasyShare</source>
-        <translation type="unfinished"></translation>
+        <translation>EasyShare</translation>
     </message>
     <message>
         <location filename="easyshare.py" line="124"/>
         <source>Easy filesharing</source>
-        <translation type="unfinished"></translation>
+        <translation>Partage de fichiers</translation>
     </message>
 </context>
 <context>
@@ -18,32 +19,32 @@
     <message>
         <location filename="easyshare.py" line="104"/>
         <source>One directory up</source>
-        <translation type="unfinished"></translation>
+        <translation>Repertoire parent</translation>
     </message>
     <message>
         <location filename="easyshare.py" line="107"/>
         <source>Choose file or directory: </source>
-        <translation type="unfinished"></translation>
+        <translation>Choisir un fichier ou un repertoire</translation>
     </message>
     <message>
         <location filename="easyshare.py" line="109"/>
         <source>Resend file</source>
-        <translation type="unfinished"></translation>
+        <translation>Réenvoyer le fichier</translation>
     </message>
     <message>
         <location filename="easyshare.py" line="109"/>
         <source>Choose file you want to resend from remote system or directory you want to browse.</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisir le fichier a réenvoyer ou le répertoire a parcourir</translation>
     </message>
     <message>
         <location filename="easyshare.py" line="115"/>
         <source>Path</source>
-        <translation type="unfinished"></translation>
+        <translation>Chemin</translation>
     </message>
     <message>
         <location filename="easyshare.py" line="116"/>
         <source>Allow JIDs</source>
-        <translation type="unfinished"></translation>
+        <translation>Permettre cet identifiant</translation>
     </message>
 </context>
 </TS>
