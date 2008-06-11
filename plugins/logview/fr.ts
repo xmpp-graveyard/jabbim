fc798690e2a9cc15cc60baf8784083f0e5fbbdcd
hanz <hanz@users.noreply.github.com>
2008-06-11 13:25:58 +0000

    logview de la france

--- plugins/logview/fr.ts
+++ plugins/logview/fr.ts
@@ -2,49 +2,57 @@
 <!DOCTYPE TS><TS version="1.1" language="fr_FR">
 <defaultcodec></defaultcodec>
 <context>
-    <name>Plugin</name>
+    <name>MainWindow</name>
     <message>
-        <location filename="easyshare.py" line="167"/>
-        <source>EasyShare</source>
-        <translation>EasyShare</translation>
+        <location filename="logWindow.ui.py" line="76"/>
+        <source>Debug log</source>
+        <translation>Déboguer le log</translation>
     </message>
     <message>
-        <location filename="easyshare.py" line="124"/>
-        <source>Easy filesharing</source>
-        <translation>Partage de fichiers</translation>
+        <location filename="logWindow.ui.py" line="77"/>
+        <source>Enable</source>
+        <translation>Activer</translation>
+    </message>
+    <message>
+        <location filename="logWindow.ui.py" line="78"/>
+        <source>Clear</source>
+        <translation>Effacer</translation>
+    </message>
+    <message>
+        <location filename="logWindow.ui.py" line="79"/>
+        <source>Execute</source>
+        <translation>Exécuter</translation>
+    </message>
+    <message>
+        <location filename="logWindow.ui.py" line="80"/>
+        <source>toolBar</source>
+        <translation>Barre d&apos; outils</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="logview.py" line="19"/>
+        <source>Extra debug window</source>
+        <translation>Fenêtre de déboguage</translation>
+    </message>
+    <message>
+        <location filename="logview.py" line="21"/>
+        <source>LogView Plugin</source>
+        <translation>LogView Plugin</translation>
     </message>
 </context>
 <context>
     <name>self.main</name>
     <message>
-        <location filename="easyshare.py" line="104"/>
-        <source>One directory up</source>
-        <translation>Repertoire parent</translation>
+        <location filename="logview.py" line="11"/>
+        <source>Notify on error?</source>
+        <translation>Notifier une erreur?</translation>
     </message>
     <message>
-        <location filename="easyshare.py" line="107"/>
-        <source>Choose file or directory: </source>
-        <translation>Choisir un fichier ou un repertoire</translation>
-    </message>
-    <message>
-        <location filename="easyshare.py" line="109"/>
-        <source>Resend file</source>
-        <translation>Réenvoyer le fichier</translation>
-    </message>
-    <message>
-        <location filename="easyshare.py" line="109"/>
-        <source>Choose file you want to resend from remote system or directory you want to browse.</source>
-        <translation>Choisir le fichier a réenvoyer ou le répertoire a parcourir</translation>
-    </message>
-    <message>
-        <location filename="easyshare.py" line="115"/>
-        <source>Path</source>
-        <translation>Chemin</translation>
-    </message>
-    <message>
-        <location filename="easyshare.py" line="116"/>
-        <source>Allow JIDs</source>
-        <translation>Permettre cet identifiant</translation>
+        <location filename="logview.py" line="61"/>
+        <source>Log</source>
+        <translation>Log</translation>
     </message>
 </context>
 </TS>
