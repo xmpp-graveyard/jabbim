6a2dab3072b5f2a8d74c235bcd14fe3b8207c240
hanz <hanz@users.noreply.github.com>
2008-06-13 15:18:04 +0000

    smajlici v home

--- widgets/abstractchatwidget.py
+++ widgets/abstractchatwidget.py
@@ -937,9 +937,9 @@ if (messageObject.scroll()==1) setTimeout(window.location='#bottom', 0);
 	
 	def webkitWrite(self,text):
 		for k,v in self.main.emoticonsWidget.smileys.iteritems():
-			text=text.replace(" "+k,'&nbsp;<img src="'+os.getcwd()+"/"+v+'"/>')
-			text=text.replace("&nbsp;"+k,'&nbsp;<img src="'+os.getcwd()+"/"+v+'"/>')
-			text=text.replace(">"+k,'><img src="'+os.getcwd()+"/"+v+'"/>')
+			text=text.replace(" "+k,'&nbsp;<img src="'+v+'"/>')
+			text=text.replace("&nbsp;"+k,'&nbsp;<img src="'v+'"/>')
+			text=text.replace(">"+k,'><img src="'+v+'"/>')
 		self.messageObject.message=unicode(text)
 		if self.ui.webkit.page().mainFrame().scrollBarValue(QtCore.Qt.Vertical)==self.ui.webkit.page().mainFrame().scrollBarMaximum(QtCore.Qt.Vertical):
 			self.messageObject.scr=1
