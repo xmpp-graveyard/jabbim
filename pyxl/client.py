c18a155b03b29c942af3b304690a649285e482b1
sef <sef@users.noreply.github.com>
2008-06-18 20:42:04 +0000

    fix in dns resolving on windows

--- pyxl/client.py
+++ pyxl/client.py
@@ -283,7 +283,7 @@ class Client(derived):
 				srv = IPConfig.IPConfig().get_dns()
 				dnssrv = []
 				for server in srv:
-					if len(server.strip())>0:
+					if len(server.strip())>0 and server.strip() != '0.0.0.0':
 						dnssrv.append((server, 53))
 				if len(dnssrv) > 0:
 					r = dns.Resolver(servers=dnssrv)
