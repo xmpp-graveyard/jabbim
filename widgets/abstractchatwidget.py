265cb69efcb37e6c4c2a1ef0ddf1f59a3581a1b9
hanz <hanz@users.noreply.github.com>
2008-06-21 13:03:30 +0000

    fungujici css styly ve webkitu na windows

--- widgets/abstractchatwidget.py
+++ widgets/abstractchatwidget.py
@@ -688,13 +688,9 @@ function showLastMessages(){
 		""" % (stylesheet,code,header,footer)
 
 
-
+		# debug... we don't need it anymore
 		#f=open(self.main.webkitThemeFactory.chatPath()+"/test.html","w")
 		#f.write(html)
-		#f.close()
-
-		#f=open(os.getcwd()+"/chatskins/candy/Incoming/Content.html","r")
-		#self.incoming=f.read()
 		#f.close()
 		if typ=="groupchat":
 			self.ui.webkit.page().mainFrame().setHtml(html,QtCore.QUrl("file:///"+self.main.webkitThemeFactory.groupchatPath()))
