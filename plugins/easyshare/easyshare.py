38d987d96050c20e192cfde6b3c6fa2c7e51b8cd
hanz <hanz@users.noreply.github.com>
2008-06-28 03:37:42 +0000

    mozna oprava easyshare

--- plugins/easyshare/easyshare.py
+++ plugins/easyshare/easyshare.py
@@ -277,7 +277,7 @@ class Plugin(plugins.PluginBase):
 		addr = share.split('/')[0]
 		if addr in self.config['dirs']:
 			if frm in self.config[addr+'-sharejids']:
-				return threads.deferToThread(os.listdir(par[0].replace(addr, self.config[addr+'-sharepath'])))
+				return threads.deferToThread(self.listdir(par[0].replace(addr, self.config[addr+'-sharepath'])))
 		return
 	
 	def listdir(self, dir):
