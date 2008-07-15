0a32f5666f83d818be808f39065ba4147fa81c35
hanz <hanz@users.noreply.github.com>
2008-07-15 08:16:32 +0000

    uprava UI pro JGames

--- plugins/jgames/jgames.py
+++ plugins/jgames/jgames.py
@@ -76,12 +76,10 @@ class gameWidget(groupchat.groupChatWidget):
 		self.ui.info.hide()
 		self.ui.selfAvatar.hide()
 		self.ui.pluginWidget.hide()
+		self.ui.users.hide()
+		self.ui.lineWidget.setMaximumHeight(45)
+		self.ui.lineWidget.setMinimumHeight(45)
 	
-	def editUser(self,nick,status,role=None,affiliation=None):
-		groupchat.groupChatWidget.editUser(self,nick,status,role,affiliation)
-		self.ui.users.setMinimumHeight((len(self.main.client.groupchats[self.jid].users)+self.ui.users.topLevelItemCount())*32)
-		self.ui.users.setMaximumHeight((len(self.main.client.groupchats[self.jid].users)+self.ui.users.topLevelItemCount())*32)
-
 	def on_remove(self):
 		self.gameObj.plugin.leaveGame(self.gameObj.gid)
 		self.gameObj.dialog.hide()
