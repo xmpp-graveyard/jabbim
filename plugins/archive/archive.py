bc920b8e5ac168313814c0c7ca081581a0a6dee3
lolek <lolek@users.noreply.github.com>
2008-08-05 22:55:05 +0000

    oprava predchzich commitu

--- plugins/archive/archive.py
+++ plugins/archive/archive.py
@@ -78,8 +78,6 @@ class FileBackend:
 
 	def saveMessage(self, to, body, typ, subject, xhtml, direction):
 		jid = to.userhost()
-		if typ=='groupchat':
-			jid=to.userhost()+"@"+to.resource
 		# get local time
 		t=time.time()
 		# get date in format YYYY-MM-DD
@@ -200,6 +198,7 @@ class FileBackend:
 			d=time.localtime()
 			newestStr=str(d[0])+"-"+str(d[1])+"-"+str(d[2])
 
+		
 		messages=self.getMessages(jid,newestStr,maxTime)
 		if not messages:
 			return []
