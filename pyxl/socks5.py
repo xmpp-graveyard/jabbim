61bd448e76e7a3d97a82d56df40d7108d36106a9
sef <sef@users.noreply.github.com>
2008-08-15 12:25:10 +0000

    bugfix

--- pyxl/socks5.py
+++ pyxl/socks5.py
@@ -598,7 +598,10 @@ class ClientFactory (protocol.ClientFactory):
 		except:
 			pass
 		if not self.status == 'unconnected':
-			self.xmpp.ft[self.xmpp_sid].finish()
+			try:
+				self.xmpp.ft[self.xmpp_sid].finish()
+			except:
+				print 'no sid'
 		protocol.ClientFactory.stopFactory (self)
 
 	def buildProtocol (self, a):
@@ -694,8 +697,8 @@ class Send(protocol.Protocol):
 		if self.ft:
 			self.ft.transfered = self.ft.transfered + len(data)
 			self.ft.client.dispatcher.publishEvent('on_ftTransfered', self.ft.sid, len(data))
-#			if self.ft.transfered == self.ft.size:
-#				self.ft.finish()
+			if self.ft.transfered == self.ft.size:
+				self.ft.finish()
 
 
 	
