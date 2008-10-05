8c79cb95e505dcf9713f9dadc643694b0fa0cae4
hanz <hanz@users.noreply.github.com>
2008-10-05 07:15:47 +0000

    oprava textu v tabech

--- jabbim.py
+++ jabbim.py
@@ -4207,7 +4207,7 @@ class mainWindow(QtGui.QMainWindow):
 
 		if self.config['theme']=="None" and not text:
 			# theme isn't used
-			text="QTabBar::tab {min-width: 16px;max-width: 16px;}"
+			text=""
 			self.ui.roster.theme=False
 			app.setStyle(self.qtStylesDefault)
 		else:
