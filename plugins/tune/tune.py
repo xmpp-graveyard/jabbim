02782e769e2d7a0108360033a745dda104f68307
michich <michich@users.noreply.github.com>
2008-11-06 01:00:25 +0000

    tune: big rewrite
    Almost complete rewrite of the tune plugin.
    
    Improvements:
     - asynchronous external command calls
     - asynchronous dbus calls (not yet for all players, TBD)
     - fully DBus signal based player handlers now possible:
       - currently implemented only for Amarok 2 (more to follow)
       - avoids periodic polling every 30 s
       - User Tune changes immediately when track changes
     - Audacious handler uses DBus instead of audtool
    
    Tested with: Amarok 2, Banshee, Exaile, Rhythmbox, Audacious
    
    Hopefully I didn't break the others.
    Testers of Winamp, Foobar2000, MPD, Amarok 1.x welcome.

--- plugins/tune/tune.py
+++ plugins/tune/tune.py
@@ -1,12 +1,9 @@
 # -*-coding: utf-8 -*-
 
-import sys, os, time, locale
+import sys, os, locale
 sys.path.append('.')
-if sys.platform != 'win32':
-	import commands
-import traceback
-from PyQt4 import QtCore, QtGui
-from twisted.python import log
+if sys.platform == 'win32':
+	import win32gui
 from include import plugins
 from twisted.internet.task import LoopingCall
 from twisted.internet.protocol import ProcessProtocol
@@ -19,35 +16,245 @@ class config:
 		self.config={}
 		if sys.platform == 'win32':
 			self.config['player']={'type':'list-single','label':self.main.tr("Player"), 'items':{'Winamp':'winamp', 'Foobar 2000':'fb2k' }, 'value':'winamp'}
-			
 		else:
 			self.config['player']={'type':'list-single','label':self.main.tr("Player"), 'items':{'MPD':'mpd', 'Amarok':'amarok', 'Amarok 2':'amarok2', 'Exaile':'exaile', 'Banshee':'banshee', 'Rhythmbox':'rhythmbox', 'Audacious':'audacious'}, 'value':'amarok'}
 
-class AmarokProcessProtocol(ProcessProtocol):
+class Player:
+	def __init__(self, plugin):
+		self.plugin = plugin
+		self.main = plugin.main
+	def clear_PEP(self, reason=None):
+		self.plugin.sendPEP({})
+	def check(self):
+		pass
+
+class Winamp(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		out = {}
+		try:
+			hWinamp = win32gui.FindWindow('Winamp v1.x', None)
+			text = win32gui.GetWindowText(hWinamp).decode(locale.getpreferredencoding())
+		except Exception, ex:
+			text = ''
+		if len(text) > 0:
+			parts = text.split(' - ')
+			if len(parts) > 1:
+				out['artist'] = parts[0].split(' ', 1)[1]
+				out['title'] = parts[1]
+			else:
+				out = {}
+			if text.find('[Stopped]')!= -1:
+				out = {}
+		self.plugin.sendPEP(out)
+
+class Foobar2000(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		out = {}
+		try:
+			hfb2k = win32gui.FindWindow('{97E27FAA-C0B3-4b8e-A693-ED7881E99FC1}', None)
+			text = win32gui.GetWindowText(hfb2k).decode(locale.getpreferredencoding())
+		except Exception, ex:
+			text = ''
+		if len(text) > 0:
+			parts = text.split(' - ')
+			if len(parts) > 1:
+				out['artist'] = parts[0]
+				out['title'] = parts[1].split('[foobar2000 v', 1)[0].strip()
+			else:
+				out = {}
+		self.plugin.sendPEP(out)
+
+class MPDProtocol(ProcessProtocol):
+	def __init__(self, plugin):
+		self.plugin = plugin
+		self.output = ''
+	def outReceived(self, output):
+		self.output += output
+	def processEnded(self, reason):
+		out = {}
+		if isinstance(reason.value, ProcessDone) and self.output.find('\n[playing]') != -1:
+			text = self.output.split('\n')[0]
+			text = text.split(' - ', 1)
+			if len(text)>1:
+				out['artist'] = text[0]
+				out['title'] = text[1]
+			else:
+				out['title'] = self.output.split('\n')[0].split('/')[-1]
+			out['length'] = self.output.split('\n')[1].split('/')[-1].split('(')[0].strip()
+		self.plugin.sendPEP(out)
+
+class MPD(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		reactor.spawnProcess(
+			MPDProcessProtocol(self.plugin),
+			'mcp', ['mcp', 'status'],
+			env=os.environ)
+
+class AmarokProtocol(ProcessProtocol):
 	def __init__(self, plugin, out, field):
 		self.plugin = plugin
 		self.out = out
 		self.field = field
-
-	# Will get called when the subprocess has data on stdout
 	def outReceived(self, data):
 		self.out[self.field] = unicode(data, "utf-8").strip()
-
-	# Will get called when the subprocess has data on stderr
-	def errReceived(self, data):
-		pass
-
-	# Will get called when the subprocess starts
-	def connectionMade(self):
-		pass
-
-	# Will get called when the subprocess ends
 	def processEnded(self, reason):
-		# ProcessDone indicates successful exit
 		if not isinstance(reason.value, ProcessDone):
 			self.out = {}
-		if (len(self.out) == 2 or self.out == {}):
+		if len(self.out) == 2 or self.out == {}:
 			self.plugin.sendPEP(self.out)
+
+class Amarok(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		out = {}
+		for field in ['artist', 'title']:
+			reactor.spawnProcess(
+				AmarokProtocol(self.plugin, out, field),
+				'dcop', ['dcop', 'amarok', 'player', field],
+				env=os.environ)
+
+class Amarok2(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+		self.sig_receivers = []
+	def got_metadata(self, song_info):
+		out = {}
+		try:
+			out['artist'] = unicode(song_info['artist'])
+			out['title'] = unicode(song_info['title'])
+		except:
+			out = {}
+		self.plugin.sendPEP(out)
+	def check(self, dummy=None):
+		# I ignore the data supplied in DBus signals {Track,Status}Change,
+		# because I couldn't make it work reliably. Instead I just always ask
+		# Amarok again what the current song is. It's slower, but works.
+		try:
+			bus = self.main.session_dbus
+			amarok_player = bus.get_object("org.mpris.amarok", "/Player")
+			amarok_player.GetMetadata(reply_handler=self.got_metadata, error_handler=self.clear_PEP)
+		except:
+			self.clear_PEP()
+	def start_listening(self):
+		bus = self.main.session_dbus
+		self.sig_receivers.append(bus.add_signal_receiver(self.check,
+			'TrackChange',  "org.freedesktop.MediaPlayer", "org.mpris.amarok", "/Player"))
+		self.sig_receivers.append(bus.add_signal_receiver(self.check,
+			'StatusChange', "org.freedesktop.MediaPlayer", "org.mpris.amarok", "/Player"))
+	def stop_listening(self):
+		for receiver in self.sig_receivers:
+			receiver.remove()
+		self.sig_receivers = []
+
+class Exaile(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		out = {}
+		try:
+			bus = self.main.session_dbus
+			exa = bus.get_object("org.exaile.DBusInterface","/DBusInterfaceObject")
+			out['artist'] = exa.get_artist()
+			out['title'] = exa.get_title()
+		except:
+			out = {}
+		self.plugin.sendPEP(out)
+
+class Banshee(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def got_metadata(self, song_info):
+		out = {}
+		try:
+			out['artist'] = song_info['artist']
+			out['title'] = song_info['name']
+		except:
+			out = {}
+		self.plugin.sendPEP(out)
+	def check(self):
+		try:
+			bus = self.main.session_dbus
+			banshee = bus.get_object("org.bansheeproject.Banshee", "/org/bansheeproject/Banshee/PlayerEngine")
+			banshee.GetCurrentTrack(reply_handler=self.got_metadata, error_handler=self.clear_PEP)
+		except:
+			self.clear_PEP()
+
+class Rhythmbox(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		out = {}
+		try:
+			bus = self.main.session_dbus
+			rhythmboxplayer = bus.get_object("org.gnome.Rhythmbox", "/org/gnome/Rhythmbox/Player")
+			rhythmboxshell = bus.get_object("org.gnome.Rhythmbox", "/org/gnome/Rhythmbox/Shell")
+			currentTrackInfo = rhythmboxshell.getSongProperties(rhythmboxplayer.getPlayingUri())
+			out['title'] = currentTrackInfo['title']
+			out['artist'] = currentTrackInfo['artist']
+		except:
+			out = {}
+		self.plugin.sendPEP(out)
+
+#class AudaciousProtocol(ProcessProtocol):
+#	def __init__(self, plugin, info, field):
+#		self.plugin = plugin
+#		self.info = info
+#		self.field = field
+#		self.info[field] = ''
+#	def outReceived(self, data):
+#		self.info[self.field] += data
+#	def processEnded(self, reason):
+#		if not isinstance(reason.value, ProcessDone):
+#			self.info = {}
+#		if len(self.info) == 3:
+#			out = {}
+#			if self.info['status'].strip() == 'playing':
+#				out['artist'] = self.info['artist'].strip()
+#				out['title'] = self.info['title'].strip()
+#			self.plugin.sendPEP(out)
+#		elif self.info == {}:
+#			self.plugin.sendPEP({})
+#
+#class Audacious(Player):
+#	def __init__(self, plugin):
+#		Player.__init__(self, plugin)
+#	def check(self):
+#		info = {}
+#		for query in [
+#				['status', ['audtool', 'playback-status']],
+#				['artist', ['audtool', 'current-song-tuple-data','artist']],
+#				['title',  ['audtool', 'current-song-tuple-data','title' ]]]:
+#			reactor.spawnProcess(
+#				AudaciousProtocol(self.plugin, info, query[0]),
+#				'audtool', query[1],
+#				env=os.environ)
+
+class Audacious(Player):
+	def __init__(self, plugin):
+		Player.__init__(self, plugin)
+	def check(self):
+		out = {}
+		try:
+			bus = self.main.session_dbus
+			audacious_player = bus.get_object("org.mpris.audacious", "/Player")
+			status = audacious_player.GetStatus()
+			# Audacious's GetStatus() does not comply with MPRIS spec, it returns a single Int32
+			if int(status) == 0:  # playing
+				song_info = audacious_player.GetMetadata()
+				out['title'] = song_info['title']
+				out['artist'] = song_info['artist']
+			else:
+				out = {}
+		except:
+			out = {}
+		self.plugin.sendPEP(out)
 
 class Plugin(plugins.PluginBase):
 	def __init__(self, main, homedir, plugindir):
@@ -55,155 +262,64 @@ class Plugin(plugins.PluginBase):
 		self.fname = 'tune'
 		self.installTranslator()
 		self.description = self.tr('Plugin for User Tune')
-		self.author = "Jiri 'Sef' Gabrys + Josef 'PepeQ' Halicek + Krzysztof 'Grom' K. + Pinky"
+		self.author = "Jiri 'Sef' Gabrys + Josef 'PepeQ' Halicek + Krzysztof 'Grom' K. + Pinky + Michal 'Michich' Schmidt"
 		self.name = self.tr('tune')
-		self.version = '0.263'
+		self.version = '0.264'
 		self.category = ['utils']
 		self.configDialog=config(self)
 		self.url = 'http://dev.jabbim.cz/jabbim'
-		self.loop = LoopingCall(self.check)
-		self.last = None
+		self.player_installed = False
 		if main:
 			self.loadConfig()
-			self.loop.start(30)
+			self.registerHandler('on_authd', self.on_authd)
+			if self.main.client.xmlstream:
+				self.on_authd()
 		else:
 			self.loadConfig(homedir)
 
+	def on_authd(self):
+		self.last = None
+		self.loop = None
+		player_map = {
+			'winamp':    Winamp,
+			'fb2k':      Foobar2000,
+			'mpd':       MPD,
+			'amarok':    Amarok,
+			'amarok2':   Amarok2,
+			'exaile':    Exaile,
+			'banshee':   Banshee,
+			'rhythmbox': Rhythmbox,
+			'audacious': Audacious
+		}
+		# create the chosen Player instance
+		self.player = player_map[self.config['player']](self)
+		try:
+			# if it can listen for events from the music player,
+			# it will implement start_listening()
+			self.player.start_listening()
+			self.player.check()
+		except AttributeError:
+			# it is polling-based
+			self.loop = LoopingCall(self.player.check)
+			self.loop.start(30)
+		self.player_installed = True
+
 	def on_remove(self):
-		self.loop.stop()
+		if self.player_installed:
+			if self.loop:
+				self.loop.stop()
+			else:
+				self.player.stop_listening()
 		self.sendPEP({})
 
+	def on_configChanged(self):
+		self.on_remove()
+		self.on_authd()
+
 	def sendPEP(self, out):
-		if out != self.last and self.main.client.xmlstream != None:
+		if self.main.client.xmlstream == None:
+			print "tune: Can't sendPEP yet, no xmlstream!"
+			return
+		if out != self.last:
 			self.main.client.sendPEP('http://jabber.org/protocol/tune', self.main.client.getTunePayload(out))
 			self.last = out
-
-	def check(self):
-		out = {}
-		if self.config['player'] == 'mpd':
-			try:
-				output = commands.getoutput('mpc status 2>/dev/null')
-			except:
-				pass
-			if output.find('\n[playing]') != -1:
-					text = output.split('\n')[0]
-					text = text.split(' - ', 1)
-					if len(text)>1:
-						out['artist'] = text[0]
-						out['title'] = text[1]
-					else:
-						out['title'] = output.split('\n')[0].split('/')[-1]
-					out['lenght'] = output.split('\n')[1].split('/')[-1].split('(')[0].strip()
-
-		elif self.config['player'] == 'exaile':
-			try:
-				import dbus
-				bus = self.main.session_dbus
-				obj = bus.get_object("org.exaile.DBusInterface","/DBusInterfaceObject")
-				exa = dbus.Interface(obj,"org.exaile.DBusInterface")
-				out['artist'] = exa.get_artist()
-				if exa.get_title() !='':
-					out['title'] = exa.get_title() #nebudu zavadet novou promenou, zbytecne.
-				else:	
-					out = {}
-			except:
-				out = {}
-
-		elif self.config['player'] == 'banshee':
-			try:
-				bus = self.main.session_dbus
-				banshee = bus.get_object("org.bansheeproject.Banshee", "/org/bansheeproject/Banshee/PlayerEngine")
-				currentTrack = banshee.GetCurrentTrack()
-				out['artist'] = currentTrack['artist']
-				out['title'] = currentTrack['name']
-			except:
-				out = {}
-
-		elif self.config['player'] == 'winamp':
-			print "getting current song from winamp"
-			try:
-				import win32gui
-				hWinamp = win32gui.FindWindow('Winamp v1.x', None)
-				text = win32gui.GetWindowText(hWinamp).decode(locale.getpreferredencoding())
-			except Exception, ex:
-				print 'Tune error: ' +unicode(ex)
-				message = unicode(traceback.format_exc())
-				print message
-				text = ''
-				out = {}
-			print "got:",[text]
-			if len(text) > 0:
-				parts = text.split(' - ')
-				if len(parts)>1:
-					out['artist'] = parts[0].split(' ', 1)[1]
-					out['title'] = parts[1]
-				else:
-					out={}
-				if text.find('[Stopped]')!= -1:
-					out = {}
-
-		elif self.config['player'] == 'rhythmbox':
-			try:
-				bus = self.main.session_dbus
-				rhythmboxplayer = bus.get_object("org.gnome.Rhythmbox", "/org/gnome/Rhythmbox/Player")
-				rhythmboxshell = bus.get_object("org.gnome.Rhythmbox", "/org/gnome/Rhythmbox/Shell")
-				currentTrackInfo = rhythmboxshell.getSongProperties(rhythmboxplayer.getPlayingUri())
-				out['title'] = currentTrackInfo['title']
-				out['artist'] = currentTrackInfo['artist']
-			except:
-				out = {}
-
-		elif self.config['player'] == 'amarok2':
-			try:
-				bus = self.main.session_dbus
-				amarok_player = bus.get_object("org.kde.amarok", "/Player")
-				song_info = amarok_player.GetMetadata()
-				out['title'] = song_info['title']
-				out['artist'] = song_info['artist']
-			except:
-				out = {}
-
-		elif self.config['player'] == 'amarok':
-			try:
-				for field in ['artist', 'title']:
-					reactor.spawnProcess(
-						AmarokProcessProtocol(self, out, field),
-						'dcop', ['dcop', 'amarok', 'player', field],
-						env=os.environ)
-				return  # PEP will be sent when the spawned processes exit
-			except:
-				out = {}
-
-		elif self.config['player'] == 'fb2k':
-			print "getting current song from foobar"
-			try:
-				import win32gui
-				hfb2k = win32gui.FindWindow('{97E27FAA-C0B3-4b8e-A693-ED7881E99FC1}', None)
-				text = win32gui.GetWindowText(hfb2k).decode(locale.getpreferredencoding())
-			except Exception, ex:
-				print 'Tune error: ' +unicode(ex)
-				message = unicode(traceback.format_exc())
-				print message
-				text = ''
-				out = {}
-			print "got:",[text]
-			if len(text) > 0:
-				parts = text.split(' - ')
-				if len(parts)>1:
-					out['artist'] = parts[0]
-					out['title'] = parts[1].split('[foobar2000 v', 1)[0].strip()
-				else:
-					out={}
-
-		elif self.config['player'] == 'audacious':
-			try:
-				output = commands.getoutput('audtool playback-status')
-				if output.find('playing') != -1:
-					text = text = commands.getoutput('audtool current-song')
-					text = text.split(' - ', 1)
-					out['artist'] = text[0]
-					out['title'] = text[1]
-			except:
-				out = {}
-
-		self.sendPEP(out)
