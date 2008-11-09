66d17c7007e16b2dd95be9994019f1f57c7ae9b4
sef <sef@users.noreply.github.com>
2008-11-09 22:33:29 +0000

    fix pro #441

--- include/utils.py
+++ include/utils.py
@@ -586,6 +586,13 @@ def replace_url(text,widget=None):
 				text+='<a href="%s" title="%s">%s</a>'%(word, word, word)+" "
 		elif word.startswith("www."):
 			text+='<a href="http://%s" title="%s">%s</a>'%(word, word, word)+" "
+		elif word.find('@') != -1:
+			wellKnown = ['jabbim.cz', 'njs.netlab.cz', 'pyco.cz', 'jabbim.sk', 'jabbim.pl', 'jabber.cz', 'jabbim.com', 'gmail.com', 'jabber.org', 'jabster.pl', 'jabber.ru']
+			print widget
+			if (widget != None and widget.main().client.hasIdentity(word.split('@')[1], 'server', 'im')) or word.split('@')[1] in wellKnown :
+				text+='<a href="xmpp:%s" title="%s">%s</a>'%(word, word, word)
+			else:
+				text+='<a href="mailto:%s" title="%s">%s</a>'%(word, word, word)
 		else:
 			text+=word+" "
 	return text[:-1]
