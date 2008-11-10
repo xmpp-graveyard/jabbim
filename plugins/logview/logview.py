e6ecf75393950f233b1b97588f59101102ec1dfd
sef <sef@users.noreply.github.com>
2008-11-10 20:22:24 +0000

    update logview (triak)

--- plugins/logview/logview.py
+++ plugins/logview/logview.py
@@ -3,12 +3,17 @@ sys.path.append('.')
 from include import plugins
 from PyQt4 import QtCore, QtGui
 from twisted.python import log
+import base64
 
 class config:
 	def __init__(self,main):
 		self.main=main
 		self.config={}
 		self.config['notify']={'type':'boolean','label':self.main.tr("Notify on error?"),'value':'False'}
+		self.config['historyMaxCount']={'type':'number-spin','label':self.main.tr("Max history entries"),'value':'50'}
+		self.config['historySave']={'type':'boolean-radio','label':main.tr('History saving type'),'value':'one','options':{'one':main.tr('always after command input'),'btn':main.tr('button clicked'),'never':main.tr('never')}}
+		self.config['historyData']={'type':'hidden','value':''}
+		self.config['__sort__']=['notify','historyMaxCount','historySave','historyData']
 		
 class Plugin(plugins.PluginBase):
 	def __init__(self, main, homedir, plugindir):
@@ -16,25 +21,36 @@ class Plugin(plugins.PluginBase):
 		self.fname = 'logview'
 		self.installTranslator()
 		self.description = self.tr('Extra debug window')
-		self.author = "Jiri 'Sef' Gabrys"
+		self.author = "Jiri 'Sef' Gabrys, Ondra 'triak' Kunc"
 		self.name = self.tr('LogView Plugin')
 		self.version = '0.042'
 		self.category = ['log', 'misc']
 		self.url = 'http://dev.jabbim.cz/jabbim'
 		self.history = []
+		self.historyPosition=-1;
 		self.configDialog=config(self)
 		if main:
 			self.loadConfig()
+			self.loadHistory()
 			self.window = self.loadWindow("%s/logWindow.ui.py" % self.pluginDir)
 			self.window.setWindowIcon(self.main.windowIcon())
 			self.log = False
 			QtCore.QObject.connect(self.window.ui.enableBox, QtCore.SIGNAL("stateChanged(int)"),self.enableToggled)
 			QtCore.QObject.connect(self.window.ui.clearButton, QtCore.SIGNAL("clicked()"),self.clearLog)
 			QtCore.QObject.connect(self.window.ui.execute, QtCore.SIGNAL("clicked()"),self.execute)
+#			short=QtGui.QShortcut("ctrl+f",self.window)
+			short=QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Up | QtCore.Qt.ControlModifier),self.window)
+			QtCore.QObject.connect(short, QtCore.SIGNAL("activated ()"),self.historyForward)
+#			short=QtGui.QShortcut("ctrl+b",self.window)
+			short=QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Down | QtCore.Qt.ControlModifier),self.window)
+			QtCore.QObject.connect(short, QtCore.SIGNAL("activated ()"),self.historyBackward)
+#			short=QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL | QtCore.Qt.Key_Enter),self.window)
+			short=QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL | QtCore.Qt.Key_Return),self.window)
+			QtCore.QObject.connect(short, QtCore.SIGNAL("activated ()"),self.execute)
+			#QtCore.Qt.Key_Enter a QtCore.Qt.CTRL
+
 		else:
 			self.loadConfig(homedir)
-	
-	
 	
 	def buildMainWindowMenu(self):
 		menu=self.mainWindowMenu()
@@ -66,6 +82,35 @@ class Plugin(plugins.PluginBase):
 	def execute(self):
 		code = unicode(self.window.ui.input.toPlainText ())
 		self.window.ui.logView.append(self.window.ui.input.toPlainText ())
-		self.history.append(code)
+		self.history.insert(0,code)
+		self.history=self.history[0:long(self.config['historyMaxCount'])]
+		self.historyPosition=-1;
+		if self.config['historySave']=='one':
+			self.historySave()
 		self.window.ui.input.setText('')
 		exec(code)
+	def historySave(self):
+		tmpHistory=[];
+		for historyItem in self.history:
+			tmpHistory.append(base64.b64encode(historyItem));
+		self.config['historyData']=tmpHistory
+		self.config.write()
+	def loadHistory(self):
+		self.history=[];
+		tmpHistory=self.config['historyData'];
+		for historyItem in tmpHistory:
+			self.history.append(base64.b64decode(historyItem))
+	def historyForward(self):
+		self.historyPosition+=1;
+		if self.historyPosition>=(len(self.history)-1):
+			self.historyPosition=len(self.history)-1;
+			self.window.ui.input.setText(self.tr('print \'history ends here\''))
+		else:
+			self.window.ui.input.setText(self.history[self.historyPosition])
+	def historyBackward(self):
+		self.historyPosition-=1;
+		if self.historyPosition<0:
+			self.historyPosition=0;
+			self.window.ui.input.setText('')
+		else:
+			self.window.ui.input.setText(self.history[self.historyPosition])
