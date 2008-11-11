63778f4e17d4d07884537403ea9e874a4413f41f
sef <sef@users.noreply.github.com>
2008-11-11 14:18:19 +0000

    bugfix

--- include/utils.py
+++ include/utils.py
@@ -599,9 +599,9 @@ def replace_url(text,widget=None):
 				text+='<a href="%s" title="%s">%s</a>'%(word, word, user.split(':')[1]+'@'+server)
 			else:
 				if (widget != None and widget.main().client.hasIdentity(server, 'server', 'im')) or server in wellKnown :
-					text+='<a href="xmpp:%s" title="%s"><img src="%s/images/16x16/apps/jabbim.png" />%s</a>'%(word, word, path, word)
+					text+='<a href="xmpp:%s" title="%s"><img src="%s/images/16x16/apps/jabbim.png" />%s</a>'%(user+'@'+server, word, path, word)
 				elif (widget != None and widget.main().client.hasIdentity(server, 'conference', 'text')) or server in wellKnownMuc:
-					text+='<a href="xmpp:%s?join" title="%s"><img src="%s/images/16x16/categories/muc.png" />%s</a>'%(word, word, path, word)
+					text+='<a href="xmpp:%s?join" title="%s"><img src="%s/images/16x16/categories/muc.png" />%s</a>'%(user+'@'+server, word, path, word)
 				else:
 					text+='<a href="mailto:%s" title="%s"><img src="%s/images/16x16/actions/message.png" />%s</a>'%(word, word, path, word)
 		else:
