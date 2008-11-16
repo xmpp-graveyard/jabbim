322623f84e46bc408ce84feabda2ed1845ca4550
sef <sef@users.noreply.github.com>
2008-11-16 19:41:50 +0000

    oprava podtrhavaci magie

--- include/utils.py
+++ include/utils.py
@@ -594,9 +594,10 @@ def replace_url(text,widget=None):
 			user, server = word.split('@',1)
 			server = server.split('/')[0]
 			path = os.getcwd()
-			if word.count('@')>1 or not ('.' in server) or (widget != None and widget.main().getJid(word) is None ):
+			if word.count('@')>1 or not ('.' in server) or (widget != None and widget.main().getJid(word) is None and not(user.startswith('xmpp:') or user.startswith('mailto:'))):
 				text += word+" "
 				continue
+
 			if user.startswith('xmpp:') or user.startswith('mailto:'):
 				text+='<a href="%s" title="%s">%s</a> '%(word, word, user.split(':')[1]+'@'+server)
 			else:
