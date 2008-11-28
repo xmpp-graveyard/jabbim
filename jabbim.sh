68b7747b188ac9ae2e06f40c0f4b0aafec3d6f61
michich <michich@users.noreply.github.com>
2008-11-28 14:22:29 +0000

    jabbim.sh: handle whitespace in the path
    Add quotes so that jabbim.sh works even if someone has a space in the
    directory name.

--- jabbim.sh
+++ jabbim.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-dir_name=$0
-if [ -L $dir_name ]; then
-  dir_name=`readlink $dir_name`
+dir_name="$0"
+if [ -L "$dir_name" ]; then
+  dir_name=`readlink "$dir_name"`
 fi
-dir_name=`dirname $dir_name`
-cd $dir_name
+dir_name=`dirname "$dir_name"`
+cd "$dir_name"
 
 python -OO jabbim.py $@
 
