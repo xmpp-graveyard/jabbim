b68e04ebcb7df16a2d18a6b51d1c3907f4c80649
pinky <pinky@users.noreply.github.com>
2008-12-14 12:17:09 +0000

    CZ locale update

--- locales/jabbim_cs.ts
+++ locales/jabbim_cs.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="cs">
+<defaultcodec></defaultcodec>
 <context>
     <name></name>
     <message>
@@ -777,7 +778,7 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="wizards/jsm_ui.py" line="69"/>
         <source>Jabbim Service Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Správce služeb Jabbim</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="74"/>
@@ -786,57 +787,57 @@ p, li { white-space: pre-wrap; }
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;h2&gt;Jabbim Service Manager&lt;/h2&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;p, li { white-space: pre-wrap; }&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;h2&gt;Správce služeb Jabbim&lt;/h2&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="75"/>
         <source>Available Services:</source>
-        <translation type="unfinished"></translation>
+        <translation>Dostupné služby:</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="76"/>
         <source>Registered</source>
-        <translation type="unfinished"></translation>
+        <translation>Registrováno</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="77"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Jméno</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="78"/>
         <source>Sort</source>
-        <translation type="unfinished"></translation>
+        <translation>Třídění</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="79"/>
         <source>1</source>
-        <translation type="unfinished">1</translation>
+        <translation>1</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="80"/>
         <source>Add</source>
-        <translation type="unfinished">Přidat</translation>
+        <translation>Přidat</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="81"/>
         <source>Register</source>
-        <translation type="unfinished"></translation>
+        <translation>Registrovat</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="82"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavit</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="83"/>
         <source>Advanced</source>
-        <translation type="unfinished">Pokročilé nastavení</translation>
+        <translation>Pokročilé nastavení</translation>
     </message>
     <message>
         <location filename="wizards/jsm_ui.py" line="84"/>
         <source>Close</source>
-        <translation type="unfinished">Zavřít</translation>
+        <translation>Zavřít</translation>
     </message>
 </context>
 <context>
@@ -5491,7 +5492,7 @@ Opravdu chcete odejít?</translation>
     <message>
         <location filename="include/utils.py" line="585"/>
         <source>Show Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazit obrázek</translation>
     </message>
 </context>
 <context>
@@ -6885,7 +6886,7 @@ Version: %3</source>
     <message>
         <location filename="widgets/rosterLiveWidget.py" line="75"/>
         <source>Show VCard</source>
-        <translation></translation>
+        <translation>Ukázat vizitku</translation>
     </message>
 </context>
 <context>
