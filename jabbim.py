dca9f79057404651038c5975c46b60557fe77e18
hanz <hanz@users.noreply.github.com>
2008-12-16 20:35:28 +0000

    oprava prejmenovani podle vCard

--- jabbim.py
+++ jabbim.py
@@ -1794,7 +1794,7 @@ class clientClass(pyxl.client.Client):
 			return
 		# Rename contact if he havent got nickname
 		contact=self.roster['users'][jid]
-		if (contact.name=="" or contact.name==contact.jid) or not contact.name:
+		if (contact.name=="" or contact.name==contact.jid) or (not contact.name or contact.name==contact.jid.split('@')[0]):
 			log.msg('trying to rename '+jid)
 			self.renameByVcard(card,jid)
 
