3e228389e60e6e53862cbf31498d9c9599f0f4c3
hanz <hanz@users.noreply.github.com>
2008-12-25 09:32:13 +0000

    oprava news.ui (nebyl vytvoreny layout v okne)

--- plugins/news/news.ui.py
+++ plugins/news/news.ui.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/hanzz/news.ui'
+# Form implementation generated from reading ui file 'plugins/news/news.ui'
 #
-# Created: Wed Dec 24 10:47:39 2008
+# Created: Thu Dec 25 10:31:54 2008
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,20 +15,18 @@ class Ui_MainWindow(object):
         MainWindow.resize(566, 497)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.widget = QtGui.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(9, 9, 1008, 602))
-        self.widget.setObjectName("widget")
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.widget)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.treeWidget = QtGui.QTreeWidget(self.widget)
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.treeWidget = QtGui.QTreeWidget(self.centralwidget)
         self.treeWidget.setMinimumSize(QtCore.QSize(150, 0))
         self.treeWidget.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.treeWidget.setHeaderHidden(True)
         self.treeWidget.setObjectName("treeWidget")
-        self.horizontalLayout_2.addWidget(self.treeWidget)
-        self.webView = QtWebKit.QWebView(self.widget)
+        self.gridLayout.addWidget(self.treeWidget, 0, 0, 1, 1)
+        self.webView = QtWebKit.QWebView(self.centralwidget)
         self.webView.setUrl(QtCore.QUrl("about:blank"))
         self.webView.setObjectName("webView")
-        self.horizontalLayout_2.addWidget(self.webView)
+        self.gridLayout.addWidget(self.webView, 0, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
