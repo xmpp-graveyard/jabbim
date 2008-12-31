21269044feb5155ba972b436cbb6b56ed20233b1
michich <michich@users.noreply.github.com>
2008-12-31 07:37:32 +0000

    first run wizard: use the main jabbimServers list
    consolidation continued

--- wizards/firststart.py
+++ wizards/firststart.py
@@ -17,7 +17,6 @@ from twisted.words.xish import domish
 from twisted.words.xish.domish import Element
 from twisted.words.protocols.jabber.xmlstream import IQ
 import weakref
-servers=["jabbim.cz","jabbim.sk","jabbim.pl","jabbim.com","jabber.cz","njs.netlab.cz"]
 
 class registrationClass(register.RegisteringClient):
 	def __init__(self,main, username, server, resource,password, port, reactor):
@@ -103,7 +102,7 @@ class firstStartWizard(QtGui.QDialog):
 		self.jid=""
 		self.state="pre"
 		
-		self.ui.servers.addItems(QtCore.QStringList([self.tr("Choose server")]+servers))
+		self.ui.servers.addItems(QtCore.QStringList([self.tr("Choose server")] + main.jabbimServers))
 		self.ui.nickname.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp("^([!#$%(-.0-9;=?a-zA-Z]+)$"),self.ui.nickname))
 		self.ui.registerButton.setEnabled(False)
 		
