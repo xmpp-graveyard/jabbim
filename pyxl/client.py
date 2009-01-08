45ef92d171cf4050e355fbd29a76bdb78f3ccee6
hanz <hanz@users.noreply.github.com>
2009-01-08 06:52:51 +0000

    zruseni reconnectu pouze pro bosh

--- pyxl/client.py
+++ pyxl/client.py
@@ -456,9 +456,9 @@ class Client(derived):
 		self.factory.addBootstrap('//event/stream/error', self._streamError)
 		self.factory.addBootstrap('//event/stream/end', self._streamEnd)
 		self.factory.addBootstrap('/*', self.bootLog)
-
-		#self.factory.clientConnectionLost = self.connectionLost
-		#self.factory.clientConnectionFailed = self.connectionFailed
+		if boshURL=="":
+			self.factory.clientConnectionLost = self.connectionLost
+			self.factory.clientConnectionFailed = self.connectionFailed
 
 		#self.connection = reactor.connectTCP(host,port, self.factory)
 		log.msg(unicode(self.factory))
