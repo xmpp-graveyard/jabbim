261bd9bbf8f6ffecfd46bef5c229f69cdd55167d
leandro <leandro@users.noreply.github.com>
2009-01-11 22:19:22 +0000

    first version

--- plugins/autoupdate/es.ts
+++ plugins/autoupdate/es.ts
@@ -1,74 +1,60 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="autoupdate.py" line="44"/>
         <source>Updates plugins</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugins de actualizaciones</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="92"/>
         <source>Autoupdate</source>
-        <translation type="unfinished"></translation>
+        <translation>Auto-actualizar</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="62"/>
         <source>Plugin updated</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin de actualización</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="62"/>
         <source>Please restart Jabbim to apply changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Por favor, reinicia Jabbim para aplicar los cambios.</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="82"/>
         <source>New version of plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Nueva versión del plugin</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="82"/>
         <source>is available</source>
-        <translation type="unfinished"></translation>
+        <translation>está disponible</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="83"/>
         <source>Do you want to update this plugin?</source>
-        <translation type="unfinished"></translation>
+        <translation>¿Quieres actualizar este plugin?</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="92"/>
         <source>New version of Jabbim is available! Get it from www.jabbim.cz</source>
-        <translation type="unfinished"></translation>
+        <translation>Hay una nueva versión de Jabbim disponible! Descárgala de www.jabbim.cz</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="93"/>
         <source>Jabbim update</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualización de Jabbim</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="93"/>
         <source>Do you want to download new version?</source>
-        <translation type="unfinished"></translation>
+        <translation>Quieres bajarte la nueva versión?</translation>
     </message>
 </context>
 <context>
     <name>self.main</name>
     <message>
-        <location filename="autoupdate.py" line="29"/>
         <source>Notify about new versions of Jabbim</source>
-        <translation type="unfinished"></translation>
+        <translation>Notificarme sobre nuevas versiones de Jabbim</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="34"/>
         <source>Notify about plugin updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Notificarme sobre actualizaciones del plugin</translation>
     </message>
     <message>
-        <location filename="autoupdate.py" line="83"/>
         <source>update</source>
-        <translation type="unfinished"></translation>
+        <translation>actualización</translation>
     </message>
 </context>
 </TS>
