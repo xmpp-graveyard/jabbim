f0746ba42123d61e401899cf4cf01e4f7fcb5900
leandro <leandro@users.noreply.github.com>
2009-01-11 22:18:48 +0000

    first version

--- plugins/archive/es.ts
+++ plugins/archive/es.ts
@@ -1,57 +1,47 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="historyBrowser.ui.py" line="74"/>
         <source>Archive browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegador de archivos</translation>
     </message>
     <message>
-        <location filename="historyBrowser.ui.py" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoy</translation>
     </message>
     <message>
-        <location filename="historyBrowser.ui.py" line="76"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Buscar</translation>
     </message>
     <message>
-        <location filename="historyBrowser.ui.py" line="77"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerrar</translation>
     </message>
 </context>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="archive.py" line="307"/>
         <source>Message Archiving</source>
-        <translation type="unfinished"></translation>
+        <translation>Archivado de mensajes</translation>
     </message>
     <message>
-        <location filename="archive.py" line="309"/>
         <source>Archive Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin de historial</translation>
     </message>
     <message>
-        <location filename="archive.py" line="484"/>
         <source>History</source>
-        <translation type="unfinished"></translation>
+        <translation>Historial</translation>
     </message>
 </context>
 <context>
     <name>self.main</name>
     <message>
-        <location filename="archive.py" line="298"/>
         <source>Number of last messages shown:</source>
-        <translation type="unfinished"></translation>
+        <translation>Número de últimos mensajes a mostar:</translation>
     </message>
     <message>
-        <location filename="archive.py" line="299"/>
         <source>Don&apos;t show messages older than:</source>
-        <translation type="unfinished"></translation>
+        <translation>No mostrar mensajes más antiguos de:</translation>
     </message>
 </context>
 </TS>
