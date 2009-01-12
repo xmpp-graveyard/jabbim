bfcfddf6ba37be464160dc80d2dfb2d6010da589
michich <michich@users.noreply.github.com>
2009-01-12 17:50:49 +0000

    PL translation for autoupdate [Grom]

--- plugins/autoupdate/pl.ts
+++ plugins/autoupdate/pl.ts
@@ -1,56 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="pl_PL">
+<defaultcodec></defaultcodec>
 <context>
     <name>Plugin</name>
     <message>
         <location filename="autoupdate.py" line="62"/>
         <source>Plugin updated</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wtyczka zaktualizowana</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="62"/>
         <source>Please restart Jabbim to apply changes.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Proszę zrestartować komunikator by wprowadzić zmiany.</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="92"/>
         <source>Autoupdate</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Autoaktualizacja</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="82"/>
         <source>New version of plugin</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nowa wersja wtyczki</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="82"/>
         <source>is available</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">jest dostępna</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="83"/>
         <source>Do you want to update this plugin?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Czy chcesz zaktualizować tę wtyczkę?</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="44"/>
         <source>Updates plugins</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Aktualizacje wtyczek</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="92"/>
         <source>New version of Jabbim is available! Get it from www.jabbim.cz</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nowa wersja Jabbima jest dostępna! Pobierz ją z www.jabbim.pl</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="93"/>
         <source>Jabbim update</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Aktualizacja Jabbima</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="93"/>
         <source>Do you want to download new version?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Czy chcesz pobrać nową wersję?</translation>
     </message>
 </context>
 <context>
@@ -58,17 +59,17 @@
     <message>
         <location filename="autoupdate.py" line="83"/>
         <source>update</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">aktualizacja</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="29"/>
         <source>Notify about new versions of Jabbim</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Powiadamiaj mnie o nowych wersjach Jabbima</translation>
     </message>
     <message>
         <location filename="autoupdate.py" line="34"/>
         <source>Notify about plugin updates</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Powiadamiaj mnie o aktualizacjach wtyczek</translation>
     </message>
 </context>
 </TS>
