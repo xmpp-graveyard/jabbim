3841d51e00d756bc2fde755d976eaba8162024e5
leandro <leandro@users.noreply.github.com>
2009-01-13 20:40:40 +0000

    first translation to spanish version

--- plugins/ibuddy/es.ts
+++ plugins/ibuddy/es.ts
@@ -1,20 +1,22 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="ibuddy.py" line="17"/>
         <source>iBuddy</source>
-        <translation type="unfinished"></translation>
+        <translation>iBuddy</translation>
     </message>
     <message>
-        <location filename="ibuddy.py" line="15"/>
         <source>Get your iBuddy working with Jabbim
 
 iBuddy is a little figure-shaped USB-connected toy which reacts to chat events by changing color (see http://www.i-buddy.com/).
 
 You need to have the &apos;pybuddy&apos; daemon installed and running before loading this plugin. You can get pybuddy from http://code.google.com/p/pybuddy/</source>
-        <translation type="unfinished"></translation>
+        <translation>¡Haz funcionar tu iBuddy con Jabbim!
+
+iBuddy es un pequeño muñeco USB que reacciona a los eventos de chat cambiando de color y moviendo sus alas. También se enciende su corazón. (Ver http://www.i-buddy.com/).
+
+Antes de cargar este plugin necesitas tener el daemon de &apos;pybuddy&apos; instalado y corriendo.
+Puedes obtener el código fuente de pybuddy en el sitio web http://code.google.com/p/pybuddy/</translation>
     </message>
 </context>
 </TS>
