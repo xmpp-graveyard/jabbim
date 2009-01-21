b1d647312b0a063ebb289ee29e687c50f04527f3
leandro <leandro@users.noreply.github.com>
2009-01-21 21:37:53 +0000

    first spanish version

--- plugins/dcoper/es.ts
+++ plugins/dcoper/es.ts
@@ -1,21 +1,17 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>self.main</name>
     <message>
-        <location filename="dcoper.py" line="16"/>
         <source>Format of message. %artist is replaced by artist&apos;s name, %title by track name and so on for %album, %genre, %year, %type and %totalTime</source>
-        <translation type="unfinished"></translation>
+        <translation>Formato del mensaje. %artist se reemplazará por el nombre del artista, %title por el nombre de la canción y así también para %album, %genre, %year, %type y %totalTime</translation>
     </message>
     <message>
-        <location filename="dcoper.py" line="21"/>
         <source>Message when amaroK is not running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mensaje cuando amaroK no está corriendo.</translation>
     </message>
     <message>
-        <location filename="dcoper.py" line="26"/>
         <source>Message when nothing is being played in amaroK.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mensaje cuando no se está reproduciendo nada en amaroK.</translation>
     </message>
 </context>
 </TS>
