4b825198dda828cb835e4a35d111357a7ff205d3
leandro <leandro@users.noreply.github.com>
2009-01-24 22:07:33 +0000

    first version spanish

--- plugins/festival/es.ts
+++ plugins/festival/es.ts
@@ -1,62 +1,53 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="festival.py" line="86"/>
         <source>Festival voice synthesizer</source>
-        <translation type="unfinished"></translation>
+        <translation>Sintetizador de voz Festival</translation>
     </message>
     <message>
-        <location filename="festival.py" line="136"/>
         <source>%s writes: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>%s escribe: %s</translation>
     </message>
     <message>
-        <location filename="festival.py" line="84"/>
         <source>Reads messages with synthesized voice via Festival
 
 For this plugin to work, you need to have Festival installed and found in $PATH. To verify that Festival works for you, run the command:
 echo Hello | festival --tts</source>
-        <translation type="unfinished"></translation>
+        <translation>Lee mensajes con voz sintetizada a través de Festival
+Para que funcione este plugin, necesitas tener Festival instalar y estar en el $PATH. Para verificar que te  funciona Festival, ejecuta el comando:
+echo Hola | festival --tts</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="festival.py" line="13"/>
         <source>Initial state after startup</source>
-        <translation type="unfinished"></translation>
+        <translation>Estado inicial después del arranque</translation>
     </message>
     <message>
-        <location filename="festival.py" line="13"/>
         <source>off</source>
-        <translation type="unfinished"></translation>
+        <translation>apagado</translation>
     </message>
     <message>
-        <location filename="festival.py" line="13"/>
         <source>on</source>
-        <translation type="unfinished"></translation>
+        <translation>activado</translation>
     </message>
     <message>
-        <location filename="festival.py" line="14"/>
         <source>previous state</source>
-        <translation type="unfinished"></translation>
+        <translation>estado previo</translation>
     </message>
     <message>
-        <location filename="festival.py" line="14"/>
         <source>read chats</source>
-        <translation type="unfinished"></translation>
+        <translation>leer chats</translation>
     </message>
     <message>
-        <location filename="festival.py" line="15"/>
         <source>read MUC messages directed at me</source>
-        <translation type="unfinished"></translation>
+        <translation>leer mensajes MUC dirigidos a mi</translation>
     </message>
     <message>
-        <location filename="festival.py" line="16"/>
         <source>read other MUC messages</source>
-        <translation type="unfinished"></translation>
+        <translation>leer otros mensajes de MUC</translation>
     </message>
 </context>
 </TS>
