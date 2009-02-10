9ede8dd8d6ceb4cf6f7b27d2644dded6cc9f7c86
leandro <leandro@users.noreply.github.com>
2009-02-10 19:50:15 +0000

    first spanish version

--- plugins/responder/es.ts
+++ plugins/responder/es.ts
@@ -1,49 +1,45 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="responder.py" line="26"/>
         <source>CAREFUL!
 Do not enable this plugin unless you intend to cut down a little on your ICQ usage.
 
 ICQ auto responder is meant for the ICQ transport users who want to quit ICQ for good.
 If you enable this plugin, contacts trying to write to you over ICQ will receive an automatic reply telling them how to reach you properly. You will NOT see their messages. The exact text of the automatic reply is customizable.</source>
-        <translation type="unfinished"></translation>
+        <translation>¡Cuidado!
+No habilites este plugin si no pretendes dejar de usar ICQ
+El auto-respondedor de ICQ está diseñado para la pasarela de ICQ de los usuarios que quieren abandonar ICQ.
+Si habilitas este plugin, los contactos que intenten escribirte por ICQ recibirán una respuesta automática diciéndoles como contactar contigo de la mejor manera. Tu NO verás sus mensajes. El texto exacto de la respuesta automática es personalizable.</translation>
     </message>
     <message>
-        <location filename="responder.py" line="52"/>
         <source>ICQ Responder</source>
-        <translation type="unfinished"></translation>
+        <translation>Respondedor de ICQ</translation>
     </message>
     <message>
-        <location filename="responder.py" line="42"/>
         <source>Show statistics</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar estadísticas</translation>
     </message>
     <message>
-        <location filename="responder.py" line="52"/>
         <source>Messages sent: </source>
-        <translation type="unfinished"></translation>
+        <translation>Mensajes enviados:</translation>
     </message>
 </context>
 <context>
     <name>self.main</name>
     <message>
-        <location filename="responder.py" line="12"/>
         <source>Auto reply</source>
-        <translation type="unfinished"></translation>
+        <translation>Auto contestar</translation>
     </message>
     <message>
-        <location filename="responder.py" line="13"/>
         <source>Allow JIDs</source>
-        <translation type="unfinished"></translation>
+        <translation>JIDs permitidos</translation>
     </message>
     <message>
-        <location filename="responder.py" line="13"/>
         <source>This is an automatic reply. This ICQ number has been discontinued by its owner, who now uses Jabber exclusively. You can contact him/her on the JabberID [JID]. If you do not know how, visit http://www.jabbim.com/services-start.html
 Have a nice day.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ésta es una contestación automática. Este número de ICQ ha sido discontinuado por su dueño, quien ahora usa Jabber exclusivamente. Puedes contactarle/la en el JID de Jabber [JID]. Si no sabes cómo, visita http://www.jabbim.com/services-start.html
+Que pases un buen día.</translation>
     </message>
 </context>
 </TS>
