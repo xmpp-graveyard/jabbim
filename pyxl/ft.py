b467723ad4408206f5e91270ddaa8063a28089a1
sef <sef@users.noreply.github.com>
2009-04-07 22:34:29 +0000

    dalsi FT upravy

--- pyxl/ft.py
+++ pyxl/ft.py
@@ -744,7 +744,8 @@ class Jingle:
 			trans = jingle.FTTransport()
 			trans.fromString('urn:xmpp:tmp:jingle:transports:ibb')
 			content = jingle.Content('initiator',  'file offer',  'urn:xmpp:tmp:jingle:apps:file-transfer', trans ,  props)
-			self.jingleSession.contentReplace(content)
+			if self.jingleSession.role != 'initiator':
+				self.jingleSession.contentReplace(content)
 			self.transportReady = True
 			return True
 		self.br = True
