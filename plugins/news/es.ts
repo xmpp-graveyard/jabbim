25d32964e61d4107ea9e86ad279d050fdffe53eb
leandro <leandro@users.noreply.github.com>
2009-06-11 18:21:39 +0000

    latest translation to spanish

--- plugins/news/es.ts
+++ plugins/news/es.ts
@@ -1,21 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
 <context>
     <name>self.main</name>
     <message>
         <location filename="news.py" line="15"/>
         <source>Notify in tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Notificar en la bandeja</translation>
     </message>
     <message>
         <location filename="news.py" line="16"/>
         <source>Show window on new</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar ventana en nuevo</translation>
     </message>
     <message>
         <location filename="news.py" line="52"/>
         <source>Show news</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar noticias</translation>
     </message>
 </context>
 </TS>
