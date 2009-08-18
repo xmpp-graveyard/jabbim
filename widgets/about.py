125aee8dc16dedf1f6bf699bb179d35af6346362
michich <michich@users.noreply.github.com>
2009-08-18 18:06:33 +0000

    about dialog: reimplement Pinky's design using proper layouts
    It looks similar and is resizable.

--- widgets/about.py
+++ widgets/about.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'about.ui'
+# Form implementation generated from reading ui file 'widgets/about.ui'
 #
-# Created: Tue Aug 18 18:59:52 2009
-#      by: PyQt4 UI code generator 4.5.2
+# Created: Tue Aug 18 20:07:46 2009
+#      by: PyQt4 UI code generator 4.5.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,106 +12,127 @@ from PyQt4 import QtCore, QtGui
 class Ui_about_window(object):
     def setupUi(self, about_window):
         about_window.setObjectName("about_window")
-        about_window.resize(519, 326)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("images/16x16/apps/jabbim.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        about_window.setWindowIcon(icon)
-        self.gridlayout = QtGui.QGridLayout(about_window)
-        self.gridlayout.setMargin(9)
-        self.gridlayout.setSpacing(6)
-        self.gridlayout.setObjectName("gridlayout")
-        self.widget = QtGui.QWidget(about_window)
+        about_window.resize(557, 362)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(about_window.sizePolicy().hasHeightForWidth())
+        about_window.setSizePolicy(sizePolicy)
+        self.verticalLayout_3 = QtGui.QVBoxLayout(about_window)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.widget_3 = QtGui.QWidget(about_window)
+        self.widget_3.setObjectName("widget_3")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.widget_3)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.widget = QtGui.QWidget(self.widget_3)
         self.widget.setObjectName("widget")
-        self.gridlayout1 = QtGui.QGridLayout(self.widget)
-        self.gridlayout1.setMargin(0)
-        self.gridlayout1.setSpacing(0)
-        self.gridlayout1.setObjectName("gridlayout1")
-        self.widget_2 = QtGui.QWidget(self.widget)
-        self.widget_2.setObjectName("widget_2")
-        self.label_10 = QtGui.QLabel(self.widget_2)
-        self.label_10.setGeometry(QtCore.QRect(340, 190, 80, 32))
-        self.label_10.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_10.setObjectName("label_10")
-        self.label_2 = QtGui.QLabel(self.widget_2)
-        self.label_2.setGeometry(QtCore.QRect(90, 80, 80, 32))
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_2.setObjectName("label_2")
-        self.label = QtGui.QLabel(self.widget_2)
-        self.label.setGeometry(QtCore.QRect(90, 0, 80, 32))
+        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtGui.QLabel(self.widget)
         self.label.setTextFormat(QtCore.Qt.RichText)
         self.label.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
         self.label.setOpenExternalLinks(True)
         self.label.setObjectName("label")
-        self.label_6 = QtGui.QLabel(self.widget_2)
-        self.label_6.setGeometry(QtCore.QRect(290, 90, 188, 105))
-        self.label_6.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_6.setObjectName("label_6")
-        self.label_8 = QtGui.QLabel(self.widget_2)
-        self.label_8.setGeometry(QtCore.QRect(70, 110, 107, 30))
+        self.verticalLayout.addWidget(self.label)
+        self.version = QtGui.QLabel(self.widget)
+        self.version.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.version.setObjectName("version")
+        self.verticalLayout.addWidget(self.version)
+        spacerItem = QtGui.QSpacerItem(20, 33, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.label_2 = QtGui.QLabel(self.widget)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.label_8 = QtGui.QLabel(self.widget)
         self.label_8.setAlignment(QtCore.Qt.AlignCenter)
         self.label_8.setObjectName("label_8")
-        self.label_9 = QtGui.QLabel(self.widget_2)
-        self.label_9.setGeometry(QtCore.QRect(70, 140, 129, 31))
+        self.verticalLayout.addWidget(self.label_8)
+        spacerItem1 = QtGui.QSpacerItem(20, 34, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem1)
+        self.label_9 = QtGui.QLabel(self.widget)
         self.label_9.setAlignment(QtCore.Qt.AlignCenter)
         self.label_9.setObjectName("label_9")
-        self.mucLink = QtGui.QLabel(self.widget_2)
-        self.mucLink.setGeometry(QtCore.QRect(40, 160, 194, 41))
+        self.verticalLayout.addWidget(self.label_9)
+        self.mucLink = QtGui.QLabel(self.widget)
         self.mucLink.setAlignment(QtCore.Qt.AlignCenter)
         self.mucLink.setOpenExternalLinks(False)
         self.mucLink.setObjectName("mucLink")
-        self.label_12 = QtGui.QLabel(self.widget_2)
-        self.label_12.setGeometry(QtCore.QRect(50, 190, 171, 16))
+        self.verticalLayout.addWidget(self.mucLink)
+        self.label_12 = QtGui.QLabel(self.widget)
         self.label_12.setAlignment(QtCore.Qt.AlignCenter)
         self.label_12.setOpenExternalLinks(True)
         self.label_12.setObjectName("label_12")
-        self.version = QtGui.QLabel(self.widget_2)
-        self.version.setGeometry(QtCore.QRect(50, 30, 161, 61))
-        self.version.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
-        self.version.setObjectName("version")
-        self.label_4 = QtGui.QLabel(self.widget_2)
-        self.label_4.setGeometry(QtCore.QRect(300, 30, 171, 30))
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
-        self.label_5 = QtGui.QLabel(self.widget_2)
-        self.label_5.setGeometry(QtCore.QRect(320, 60, 131, 32))
-        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_5.setObjectName("label_5")
-        self.label_13 = QtGui.QLabel(self.widget_2)
-        self.label_13.setGeometry(QtCore.QRect(270, 220, 209, 41))
-        self.label_13.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
-        self.label_13.setObjectName("label_13")
+        self.verticalLayout.addWidget(self.label_12)
+        self.horizontalLayout.addWidget(self.widget)
+        self.widget_2 = QtGui.QWidget(self.widget_3)
+        self.widget_2.setObjectName("widget_2")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.widget_2)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.label_3 = QtGui.QLabel(self.widget_2)
-        self.label_3.setGeometry(QtCore.QRect(330, 0, 117, 32))
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
-        self.pushButton = QtGui.QPushButton(self.widget_2)
-        self.pushButton.setGeometry(QtCore.QRect(190, 270, 101, 27))
-        self.pushButton.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.pushButton.setAutoDefault(False)
-        self.pushButton.setDefault(True)
-        self.pushButton.setFlat(False)
-        self.pushButton.setObjectName("pushButton")
-        self.gridlayout1.addWidget(self.widget_2, 0, 0, 1, 1)
-        self.gridlayout.addWidget(self.widget, 0, 0, 1, 1)
+        self.verticalLayout_2.addWidget(self.label_3)
+        self.label_4 = QtGui.QLabel(self.widget_2)
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout_2.addWidget(self.label_4)
+        spacerItem2 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem2)
+        self.label_5 = QtGui.QLabel(self.widget_2)
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_2.addWidget(self.label_5)
+        self.label_6 = QtGui.QLabel(self.widget_2)
+        self.label_6.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_6.setObjectName("label_6")
+        self.verticalLayout_2.addWidget(self.label_6)
+        spacerItem3 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem3)
+        self.label_10 = QtGui.QLabel(self.widget_2)
+        self.label_10.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_10.setObjectName("label_10")
+        self.verticalLayout_2.addWidget(self.label_10)
+        self.label_13 = QtGui.QLabel(self.widget_2)
+        self.label_13.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.label_13.setObjectName("label_13")
+        self.verticalLayout_2.addWidget(self.label_13)
+        self.horizontalLayout.addWidget(self.widget_2)
+        self.verticalLayout_3.addWidget(self.widget_3)
+        spacerItem4 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_3.addItem(spacerItem4)
+        self.buttonBox = QtGui.QDialogButtonBox(about_window)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.buttonBox.sizePolicy().hasHeightForWidth())
+        self.buttonBox.setSizePolicy(sizePolicy)
+        self.buttonBox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Close)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout_3.addWidget(self.buttonBox)
 
         self.retranslateUi(about_window)
-        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL("clicked()"), about_window.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), about_window.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), about_window.reject)
         QtCore.QMetaObject.connectSlotsByName(about_window)
 
     def retranslateUi(self, about_window):
         about_window.setWindowTitle(QtGui.QApplication.translate("about_window", "About Jabbim", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_10.setText(QtGui.QApplication.translate("about_window", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"     p, li { white-space: pre-wrap; }\n"
-"     </style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
-"     <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Thanks to:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("about_window", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"     p, li { white-space: pre-wrap; }\n"
-"     </style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
-"     <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">License:</span> </p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("about_window", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"     p, li { white-space: pre-wrap; }\n"
-"     </style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
-"     <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Version:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("about_window", "<b>Version:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.version.setText(QtGui.QApplication.translate("about_window", "$VERSION", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("about_window", "<b>License:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_8.setText(QtGui.QApplication.translate("about_window", "GNU GPL version 2\n"
+"     ", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_9.setText(QtGui.QApplication.translate("about_window", "<b>Where you can find us:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.mucLink.setText(QtGui.QApplication.translate("about_window", "Conference: <a href=\'xmpp:jabbim@conf.netlab.cz?join\'>jabbim@conf.netlab.cz</a>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_12.setText(QtGui.QApplication.translate("about_window", "Web: <a href=\"http://dev.jabbim.cz/jabbim\">http://dev.jabbim.cz/jabbim</a>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("about_window", "<b>Main developers:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("about_window", "Jan \'HanzZ\' Kaluža\n"
+"     Jiří­ \'Sef\' Gabryš", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_5.setText(QtGui.QApplication.translate("about_window", "<b>Other developers:</b>", None, QtGui.QApplication.UnicodeUTF8))
         self.label_6.setText(QtGui.QApplication.translate("about_window", "Josef \'Cornelius\' Vybí­ral\n"
 "Josef \'Pepeq\' Halíček\n"
 "Jáchym \'Kamahl\' Barvínek\n"
@@ -119,29 +140,6 @@ class Ui_about_window(object):
 "Jan \'Pinky\' Pinkas\n"
 "Zenon \'Zenek\' Kuder\n"
 "and many translators and patchers", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_8.setText(QtGui.QApplication.translate("about_window", "GNU GPL version 2\n"
-"     ", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_9.setText(QtGui.QApplication.translate("about_window", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"     p, li { white-space: pre-wrap; }\n"
-"     </style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
-"     <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Where you find us:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.mucLink.setText(QtGui.QApplication.translate("about_window", "Conference: <a href=\'xmpp:jabbim@conf.netlab.cz?join\'>jabbim@conf.netlab.cz</a>", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_12.setText(QtGui.QApplication.translate("about_window", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Arial\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Web: <a href=\"http://dev.jabbim.cz/jabbim\"><span style=\" text-decoration: underline; color:#0000ff;\">http://dev.jabbim.cz/jabbim</span></a></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.version.setText(QtGui.QApplication.translate("about_window", "$VERSION", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("about_window", "Jan \'HanzZ\' Kaluža\n"
-"     Jiří­ \'Sef\' Gabryš", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_5.setText(QtGui.QApplication.translate("about_window", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"     p, li { white-space: pre-wrap; }\n"
-"     </style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
-"     <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Other developers:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_10.setText(QtGui.QApplication.translate("about_window", "<b>Thanks to:</b>", None, QtGui.QApplication.UnicodeUTF8))
         self.label_13.setText(QtGui.QApplication.translate("about_window", "We thank all testers and bug reporters!", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("about_window", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"     p, li { white-space: pre-wrap; }\n"
-"     </style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
-"     <p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Main developers:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("about_window", "OK", None, QtGui.QApplication.UnicodeUTF8))
 
