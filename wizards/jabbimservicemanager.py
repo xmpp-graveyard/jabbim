4943c729bf0b4fc2bdde2d2d7f2f685b7f76c37c
pinky <pinky@users.noreply.github.com>
2010-03-09 12:03:02 +0000

    vyjmut facebook

--- wizards/jabbimservicemanager.py
+++ wizards/jabbimservicemanager.py
@@ -147,7 +147,7 @@ class jabbimServiceManager(QtGui.QDialog):
 
 	def loadServices(self):
 		self.ui.treeWidget.clear()
-		trans=['fb.jabbim.cz','twitter.jabbim.com','icq.netlab.cz','icq.jabber.cz','icq.jabbim.cz','sms.netlab.cz','sms.jabbim.cz']
+		trans=['twitter.jabbim.com','icq.netlab.cz','icq.jabber.cz','icq.jabbim.cz','sms.netlab.cz','sms.jabbim.cz']
 		servs=['dict.jabbim.cz','weather.jabbim.com','disk.jabbim.cz', 'news.jabbim.cz']
 		transports={}
 		services={}
@@ -172,7 +172,6 @@ class jabbimServiceManager(QtGui.QDialog):
 		self.addService(self.tr("Jabber Disk"),"disk.jabbim.cz",self.tr("<b>Jabber Disk</b><br/>Jabber Disk allows you to upload files to Jabbim server where they can be downloaded by your friends."),services["disk.jabbim.cz"])
 		self.addService(self.tr('Jabbim News'), 'news.jabbim.cz', self.tr('<b>Jabbim News</b><br/>RSS service with custom RSS feeds for Jabbim VIP users'), services['news.jabbim.cz'])
 		self.addService(self.tr("SMS Vodafone/O2"),"sms.netlab.cz",self.tr("<b>SMS Vodafone/O2</b><br/>SMS Vodafone/O2 allows you to send SMS messages straight from your Jabbim Client."),transports["sms.netlab.cz"])
-		self.addService(self.tr("Facebook"),"fb.jabbim.cz",self.tr("<b>Facebook</b><br/>Facebook transport allows you to chat with your friends who use Facebook Chat."),transports["fb.jabbim.cz"])
 		self.addService(self.tr("Twitter"),"twitter.jabbim.com",self.tr("<b>Twitter</b><br/>Twitter transport allows you to send new tweets to Twitter and chat with your friends who use Twitter."),transports["twitter.jabbim.com"])
 		if transports["icq.netlab.cz"]:
 			self.addService(self.tr("ICQ"),"icq.netlab.cz",self.tr("<b>ICQ</b><br/>ICQ transport allows you to chat with your friends who use ICQ."),transports["icq.netlab.cz"])
